0ed4cbc615e513130892f9f2aac08b94439e82e1
DigiDuncan <digiduncan@gmail.com>
2019-06-15 14:06:09 -0400

    Swapped the roll command for the new one.

--- cogs/roleplay.py
+++ cogs/roleplay.py
@@ -12,38 +12,32 @@ class RPCog:
 
 	#Die rolling command. XdY format.
 	@commands.command()
-	async def roll(self, ctx, dice):
-		await ctx.message.delete()
-		#Check to make sure the user used XdY notation.
-		try:
-			rolls, limit = map(int, dice.split('d'))
-		except Exception:
-			await ctx.send('Format has to be in XdY.')
+	async def roll(self, ctx, dString):
+		rolls = []
+		usedrolls = []
+		dSides = 0
+		dNum = 0
+		dDrops = 0
+		dTotal = 0
+		dArray = dString.split("d")
+		if len(dArray) == 2:
+			dNum = int(dArray[0])
+			dSides = int(dArray[1])
+		elif len(dArray) == 3:
+			dNum = int(dArray[0])
+			dSides = int(dArray[1])
+			dDrops = int(dArray[2])
+		else:
+			await ctx.send('Format has to be in XdY or XdYdZ.')
 			return
-		if rolls > 100:
-		#Don't let them roll a ton of dice.
-			await ctx.send('Too many dice! Try again.')
-			print(warn("User {0} tried to roll {1} dice.".format(ctx.message.author.name, rolls)))
-			return
-		if limit > 1000:
-		#Keep number of sides rational.
-			await ctx.send('Dice too big! Try again.')
-			print(warn("User {0} tried to roll a {1}-sided dice.".format(ctx.message.author.name, limit)))
-			return
-
-		#Prettify the individual roll list.
-		result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-		resultarray = result.split(", ")
-
-		#Tabulate total.
-		total = 0
-		for x in resultarray:
-			total = total + int(x)
-
-		#Output the results to chat.
-		await ctx.send('''<@{0}> rolled {1}!
-			Result(s): {2}
-			Total: {3}'''.format(ctx.message.author.id, dice, result, total))
+		for x in range(dNum):
+			currentRoll = (random.randrange(1, dSides))
+			rolls.append(currentRoll)
+		rolls.sort(key=int)
+		for x in range(dDrops, len(rolls)):
+			dTotal = dTotal + rolls[x]
+			usedrolls.append(rolls[x])
+		await ctx.send("{0} rolled {1} and got {2}!\nDice: {3}".format(ctx.message.author.name, dString, str(dTotal), str(usedrolls)))
 
 #Necessary.
 def setup(bot):
