7df33151cf5dcdc3b18d15873c76c76cb19bc01c
DigiDuncan <digiduncan@gmail.com>
2019-07-24 22:17:37 -0400

    Stop current change before starting new one.

--- cogs/change.py
+++ cogs/change.py
@@ -141,6 +141,8 @@ class ChangeCog(commands.Cog):
 			else:
 				await ctx.send("Please enter a valid change style.", delete_after=3)
 		bot = self.bot
+		try tasks[ctx.message.author.id].cancel()
+		try del tasks[ctx.message.author.id]
 		task = bot.loop.create_task(slowchangetask(ctx, style, amount, delay))
 		tasks[ctx.message.author.id] = task
 
