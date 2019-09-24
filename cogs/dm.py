e4aae1f219e45ab67f3f0f4ac91c77313a3e8a99
Natalie Fearnley <nfearnley@gmail.com>
2019-09-24 09:58:25 -0400

    Fixes for debug messages

--- cogs/dm.py
+++ cogs/dm.py
@@ -9,6 +9,26 @@ def deepgetattr(obj, attr):
     return reduce(getattr, attr.split('.'), obj)
 
 
+def dump_message(m):
+    channelName = getattr(m.channel, "name", None)
+    guildId = deepgetattr(m.channel, "guild.id", None)
+    embeds = [e.to_dict() for e in m.embeds]
+    logger.msg(
+        f"Received a message from {m.author.name}#{m.author.discriminator} that wasn't a DM: {m.content}\n"
+        f"    URL: {m.jump_url}\n"
+        f"    Guild ID: {guildId}\n"
+        f"    Channel ID: {m.channel.id}\n"
+        f"    Message ID: {m.id}\n"
+        f"    Channel name: {channelName\n}"
+        f"    Channel type: {m.channel.type.name}\n"
+        f"    Message type: {m.type.name}\n"
+        f"    Embeds: {len(m.embeds)}\n{embeds}\n"
+        f"    Attachments: {len(m.attachments)}\n"
+        f"    System content: {m.system_content}\n"
+    )
+   
+
+
 # Show an incoming DMs in console
 class DmCog(commands.Cog):
     def __init__(self, bot):
@@ -18,22 +38,7 @@ class DmCog(commands.Cog):
     async def on_message(self, m):
         if not isinstance(m.channel, discord.DMChannel):
             if not isinstance(m.author, discord.Member):
-                channelName = getattr(m.channel, "name", None)
-                guildId = deepgetattr(m.channel, "guild.id", None)
-                embeds = [e.to_dict() for e in m.embeds]
-                logger.msg(
-                    f"Received a message from {m.author.name}#{m.author.discriminator} that wasn't a DM: {m.content}\n"
-                    f"    Channel name: {channelName\n}"
-                    f"    Guild ID: {guildId}\n"
-                    f"    Channel ID: {m.channel.id}\n"}
-                    f"    Channel type: {m.channel.type.name}\n"
-                    f"    Message type: {m.type.name}\n"
-                    f"    Embeds: {len(m.embeds)}\n{embeds}\n}"
-                    f"    Attachments: {len(m.attachments)}\n"
-                    f"    System content: {m.system_content}\n"
-                    f"    URL: https://discordapp.com/channels/{guildId}/{m.channel.id}/{m.id}"
-                )
-
+                dump_message(m)
             return
         logger.msg(f"DM from {m.author.name}#{m.author.discriminator}: {m.content}")
 
