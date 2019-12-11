fe5e24c995e180b8d56ca07ebfb84d340eebcd8c
Natalie Fearnley <nfearnley@gmail.com>
2019-12-10 19:47:43 -0500

    Updated userdb to load/save JSON

--- userdb.py
+++ userdb.py
@@ -1,3 +1,5 @@
+import json
+from pathlib import Path
 from decimal import Decimal
 
 # Deprecated user array constants
@@ -15,10 +17,15 @@ defaultheight = Decimal(1754000)  # micrometers
 defaultweight = Decimal(66760000)  # milligrams
 defaultdensity = Decimal(1.0)
 
+# Map the deprecated user array constants to the new names
+DEPRECATED_NAME_MAP = ["nickname", "display", "height", "baseheight", "baseweight", "density", "unitsystem", "species"]
+
+userdbpath = Path("json_test")
+
 
 class User:
-    # __slots__ declares to python what attributes to expect. But since it's in the right order, we can also use it to look up attributes by the deprecated user info constants.
-    __slots__ = ["nickname", "display", "height", "baseheight", "baseweight", "density", "unitsystem", "species", "id"]
+    # __slots__ declares to python what attributes to expect.
+    __slots__ = ["id", "nickname", "display", "height", "baseheight", "baseweight", "density", "unitsystem", "species"]
 
     def __init__(self):
         self.id = None
@@ -32,17 +39,53 @@ class User:
         self.species = ""
 
     def __getitem__(self, key):
-        attrname = self.__slots__[key]
+        attrname = DEPRECATED_NAME_MAP[key]
         return getattr(self, attrname)
 
     def __setitem__(self, key, value):
-        attrname = self.__slots__[key]
+        attrname = DEPRECATED_NAME_MAP[key]
         return setattr(self, attrname, value)
+
+    def __str__(self):
+        return f"ID {self.id}, NICK {self.nickname}, DISP {self.display}, CHEI {self.height}, BHEI {self.baseheight}, BWEI {self.baseweight}, DENS {self.density}, UNIT {self.unitsystem}, SPEC {self.species}"
+
+    def toJSON(self):
+        return {
+            "id": self.id,
+            "nickname": self.nickname,
+            "display": self.display,
+            "height": str(self.height),
+            "baseheight": str(self.baseheight),
+            "baseweight": str(self.baseweight),
+            "density": str(self.density),
+            "unitsystem": self.unitsystem,
+            "species": self.species
+        }
+
+    @classmethod
+    def fromJSON(cls, jsondata):
+        user = User()
+        user.id = jsondata["id"]
+        user.nickname = jsondata["nickname"]
+        user.display = jsondata["display"]
+        user.height = Decimal(jsondata["height"])
+        user.baseheight = Decimal(jsondata["baseheight"])
+        user.baseweight = Decimal(jsondata["baseweight"])
+        user.density = Decimal(jsondata["density"])
+        user.unitsystem = jsondata["unitsystem"]
+        user.species = jsondata["species"]
+        return user
 
 
 def save(user):
-    pass
+    userdbpath.mkdir(exist_ok = True)
+    jsondata = user.toJSON()
+    id = user.id
+    with open(userdbpath / f"{id}.json", "w") as f:
+        json.dump(jsondata, f, indent = 4)
 
 
 def load(id):
-    pass
+    with open(userdbpath / f"{id}.json", "r") as f:
+        jsondata = json.load(f)
+    return User.fromJSON(jsondata)
