da1849c59c6a039fd944e7c79d7d46f95daab05e
Natalie Fearnley <nfearnley@gmail.com>
2019-12-10 21:41:02 -0500

    Added automatic conversion and checked to User fields

--- userdb.py
+++ userdb.py
@@ -13,9 +13,9 @@ UNIT = 6
 SPEC = 7
 
 # Defaults
-defaultheight = Decimal(1754000)  # micrometers
-defaultweight = Decimal(66760000)  # milligrams
-defaultdensity = Decimal(1.0)
+defaultheight = Decimal("1754000")  # micrometers
+defaultweight = Decimal("66760000")  # milligrams
+defaultdensity = Decimal("1.0")
 
 # Map the deprecated user array constants to the new names
 DEPRECATED_NAME_MAP = ["nickname", "display", "height", "baseheight", "baseweight", "density", "unitsystem", "species"]
@@ -25,19 +25,68 @@ userdbpath = Path("../users")
 
 class User:
     # __slots__ declares to python what attributes to expect.
-    __slots__ = ["id", "nickname", "display", "height", "baseheight", "baseweight", "density", "unitsystem", "species"]
+    __slots__ = ["id", "nickname", "display", "_height", "_baseheight", "_baseweight", "_density", "_unitsystem", "species"]
 
     def __init__(self):
         self.id = None
         self.nickname = None
         self.display = True
-        self.height = Decimal("1754000")
-        self.baseheight = Decimal("1754000")
-        self.baseweight = Decimal("66760000")
-        self.density = Decimal("1")
-        self.unitsystem = "m"
-        self.species = ""
+        self._height = defaultheight
+        self._baseheight = defaultheight
+        self._baseweight = defaultweight
+        self._density = defaultdensity
+        self._unitsystem = "m"
+        self.species = None
 
+    def __str__(self):
+        return f"ID {self.id}, NICK {self.nickname}, DISP {self.display}, CHEI {self.height}, BHEI {self.baseheight}, BWEI {self.baseweight}, DENS {self.density}, UNIT {self.unitsystem}, SPEC {self.species}"
+
+    # Setters/getters to automatically force numeric values to be stored as Decimal
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        self._height = Decimal(value)
+
+    @property
+    def baseheight(self):
+        return self._baseheight
+
+    @baseheight.setter
+    def baseheight(self, value):
+        self._baseheight = Decimal(value)
+
+    @property
+    def baseweight(self):
+        return self._baseweight
+
+    @baseweight.setter
+    def baseweight(self, value):
+        self._baseweight = Decimal(value)
+
+    @property
+    def density(self):
+        return self._density
+
+    @density.setter
+    def density(self, value):
+        self._density = Decimal(value)
+
+    # Check that unitsystem is valid and lowercase
+    @property
+    def unitsystem(self):
+        return self._unitsystem
+
+    @unitsystem.setter
+    def unitsystem(self, value):
+        value = value.lower()
+        if value not in ["m", "u"]:
+            raise ValueError(f"Invalid unitsystem: '{value}'")
+        self._unitsystem = value
+
+    # Act like an array for legacy usage
     def __getitem__(self, key):
         attrname = DEPRECATED_NAME_MAP[key]
         return getattr(self, attrname)
@@ -46,9 +95,7 @@ class User:
         attrname = DEPRECATED_NAME_MAP[key]
         return setattr(self, attrname, value)
 
-    def __str__(self):
-        return f"ID {self.id}, NICK {self.nickname}, DISP {self.display}, CHEI {self.height}, BHEI {self.baseheight}, BWEI {self.baseweight}, DENS {self.density}, UNIT {self.unitsystem}, SPEC {self.species}"
-
+    # Return an python dictionary for json exporting
     def toJSON(self):
         return {
             "id": self.id,
@@ -62,6 +109,7 @@ class User:
             "species": self.species
         }
 
+    # Create a new object from a python dictionary imported using json
     @classmethod
     def fromJSON(cls, jsondata):
         user = User()
