eb648cadd6a7d55cd397bc23e15dec72f73e71f5
DigiDuncan <digiduncan@gmail.com>
2019-12-11 20:50:52 -0500

    Working on modules. (moving to laptop lol)

--- digierror.py
+++ digierror.py
@@ -2,30 +2,52 @@ import digiformatter as df
 
 SUCCESS = "success"
 USER_NOT_FOUND = "unf"
+USER_IS_BOT = "uib"
+USER_IS_WEBHOOK = "uiw"
+USER_IS_OWNER = "uio"
+USER_NOT_REGISTERED = "unr"
 CHANGE_VALUE_IS_ZERO = "cvi0"
 CHANGE_VALUE_IS_ONE = "cvi1"
 CHANGE_METHOD_INVALID = "cmi"
 CANNOT_SAVE_WITHOUT_ID = "cswi"
+MESSAGE_WAS_DM = "mwdm"
+NO_PERMISSIONS = "np"
 
 
-async def throw(ctx, code, delete=0):
+async def throw(code, **kwargs):
+    delete = 0
+    if 'delete' in kwargs:
+        delete = kwargs['delete']
+
     if code == SUCCESS:
         return
+    if code == USER_IS_BOT:
+        pass
+    if code == USER_IS_WEBHOOK:
+        pass
+    if code == USER_IS_OWNER:
+        pass
+    if code == USER_NOT_REGISTERED:
+        pass
     if code == USER_NOT_FOUND:
-        df.warn(f"User {ctx.message.author.id} not found.")
-        await ctx.send("Sorry! You aren't registered with SizeBot.\n"
-                       "To register, use the `&register` command.", delete_after=delete)
+        df.warn(f"User {kwargs['ctx'].message.author.id} not found.")
+        await kwargs['ctx'].send("Sorry! You aren't registered with SizeBot.\n"
+                                 "To register, use the `&register` command.", delete_after=delete)
     if code == CHANGE_VALUE_IS_ZERO:
-        df.warn(f"User {ctx.message.author.id} tried to change by zero.")
-        await ctx.send("Nice try.\n"
-                       "You can't change by zero. :stuck_out_tounge:", delete_after=delete)
+        df.warn(f"User {kwargs['ctx'].message.author.id} tried to change by zero.")
+        await kwargs['ctx'].send("Nice try.\n"
+                                 "You can't change by zero. :stuck_out_tounge:", delete_after=delete)
     if code == CHANGE_VALUE_IS_ONE:
-        df.warn(f"User {ctx.message.author.id} tried to change by one.")
-        await ctx.send("You can't change by one.\n"
-                       "This is because changing by one doesn't change the value of your attribute, and especially with slowchange tasks, this clogs up SizeBot's memory.", delete_after=delete)
+        df.warn(f"User {kwargs['ctx'].message.author.id} tried to change by one.")
+        await kwargs['ctx'].send("You can't change by one.\n"
+                                 "This is because changing by one doesn't change the value of your attribute, and especially with slowchange tasks, this clogs up SizeBot's memory.", delete_after=delete)
     if code == CHANGE_METHOD_INVALID:
-        df.warn(f"User {ctx.message.author.id} tried to use an invalid changing method.")
-        await ctx.send("Your change method is invalid. Valid methods are add, subtract, multiply, or divide (and aliases for these modes.)", delete_after=delete)
+        df.warn(f"User {kwargs['ctx'].message.author.id} tried to use an invalid changing method.")
+        await kwargs['ctx'].send("Your change method is invalid. Valid methods are add, subtract, multiply, or divide (and aliases for these modes.)", delete_after=delete)
     if code == CANNOT_SAVE_WITHOUT_ID:
         df.crit(f"Cannot save user without ID!")
         raise ValueError("Cannot save user without ID!")
+    if code == MESSAGE_WAS_DM:
+        df.warn(f"Tried to update user {kwargs['user'].id} ({kwargs['user'].name})'s nick, but the message was a DM (and hence there is no nickname to update.)")
+    if code == NO_PERMISSIONS:
+        df.crit("Missing permssions!")
