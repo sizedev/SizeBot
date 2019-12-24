4c28358fcfef7cf60c958828ab33afd7b1c5012a
Natalie Fearnley <nfearnley@gmail.com>
2019-12-24 05:02:39 -0500

    Added support for missing ids.txt file

--- sizebot/iddb.py
+++ sizebot/iddb.py
@@ -7,8 +7,11 @@ def loadIds():
     # IDs are stored in text/ids.txt.
     # The format is one ID per line, in the form {id}:{name}
     # This file is not tracked by Git.
-    with io.open(conf.idfilepath, "r", encoding = "utf-8") as idfile:
-        ids = {name: id for id, name in [line.rstrip().split(":", 1) for line in idfile]}
+    try:
+        with io.open(conf.idfilepath, "r", encoding = "utf-8") as idfile:
+            ids = {name: id for id, name in [line.rstrip().split(":", 1) for line in idfile]}
+    except FileNotFoundError:
+        ids = dict()
     return ids
 
 
