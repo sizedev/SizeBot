2d2536c31876ce94a5f09f858b916a92aae52ba5
Natalie Fearnley <nfearnley@gmail.com>
2019-12-25 21:14:31 -0500

    Added defaults to &register, added reaction confirmation for unregistering, various fixes

--- sizebot/cogs/register.py
+++ sizebot/cogs/register.py
@@ -1,4 +1,5 @@
 import random
+import asyncio
 
 from discord.ext import commands
 from discord.utils import get
@@ -43,19 +44,19 @@ class RegisterCog(commands.Cog):
 
     # Registers a user for SizeBot
     @commands.command()
-    async def register(self, ctx, nick: str, display: str, currentheight: str, baseheight: str, baseweight: str, units: str, species: str = None):
+    async def register(self, ctx, nick: str, display: str = "y", currentheight: str = "5ft10in", baseheight: str = "5ft10in", baseweight: str = "180lb", unitsystem: str = "m", species: str = None):
         readable = "CH {0}, BH {1}, BW {2}".format(currentheight, baseheight, baseweight)
         df.warn("New user attempt! Nickname: {0}, Display: {1}".format(nick, display))
         print(readable)
 
-        currentheightSV = digiSV.toWV(currentheight)
-        baseheightSV = digiSV.toWV(baseheight)
+        currentheightSV = digiSV.toSV(currentheight)
+        baseheightSV = digiSV.toSV(baseheight)
         baseweightWV = digiSV.toWV(baseweight)
 
         # Already registered
         if userdb.exists(ctx.message.author.id):
-            await ctx.send("""Sorry! You already registered with SizeBot.
-    To unregister, use the `&unregister` command.""", delete_after = 10)
+            await ctx.send("Sorry! You already registered with SizeBot.\n"
+                           "To unregister, use the `&unregister` command.", delete_after = 10)
             df.warn("User already registered on user registration: {1}.".format(ctx.message.author))
             return
 
@@ -71,41 +72,41 @@ class RegisterCog(commands.Cog):
             return
 
         # Invalid unit value
-        if units.lower() not in ["m", "u"]:
-            df.warn("units was {0}, must be M or U.".format(units))
-            await ctx.send("Units must be `M` or `U`.", delete_after = 5)
+        if unitsystem.lower() not in ["m", "u"]:
+            df.warn("unitsystem was {0}, must be M or U.".format(unitsystem))
+            await ctx.send("Unitsystem must be `M` or `U`.", delete_after = 5)
             return
 
-        # Success.
-        if species is None:
-            species = "None"
-
         userdata = userdb.User()
+        userdata.id = ctx.message.author.id
         userdata.nickname = nick
         userdata.display = display
-        userdata.height = digiSV.toSV(currentheight)
-        userdata.baseheight = digiSV.toSV(baseheight)
-        userdata.baseweight = digiSV.toWV(baseweight)
-        userdata.units = units
+        userdata.height = currentheightSV
+        userdata.baseheight = baseheightSV
+        userdata.baseweight = baseweightWV
+        userdata.unitsystem = unitsystem
         userdata.species = species
 
         userdb.save(userdata)
 
         await addUserRole(ctx.message.author)
 
-        df.warn("Made a new user: {0}!".format(ctx.message.author))
+        df.warn(f"Made a new user: {ctx.message.author}!")
         print(userdata)
-        await ctx.send("Registered <@{0}>. {1}.".format(ctx.message.author.id, readable), delete_after = 5)
+        await ctx.send(f"Registered <@{ctx.message.author.id}>. {readable}.", delete_after = 5)
 
     @register.error
     async def register_handler(self, ctx, error):
-        # Check if required argument is missing.
+        # Check if required argument is missing
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send("""Not enough variables for `register`.
-    Use `&register [nick] [display (Y/N)] [currentheight] [baseheight] [baseweight] [M/U]`.""", delete_after = 30)
+            await ctx.send("Not enough variables for `register`.\n"
+                           "Use `&register [nick] [display (Y/N)] [currentheight] [baseheight] [baseweight] [M/U]`.",
+                           delete_after = 30)
+            return
+        raise error
 
     @commands.command()
-    async def unregister(self, ctx, code = None):
+    async def unregister(self, ctx):
         # User file missing
         if not userdb.exists(ctx.message.author.id):
             df.warn(f"User {ctx.message.author.id} not registered with SizeBot, but tried to unregister anyway.")
@@ -113,28 +114,35 @@ class RegisterCog(commands.Cog):
                            "To register, use the `&register` command.", delete_after = 5)
             return
 
-        if code is None or hexcode is None:
-            regenHexCode()
-            await ctx.send("To unregister, use the `&unregister` command and the following code.\n"
-                           f"`{hexcode}`", delete_after = 30)
-            return
+        unregisterIcon = "❌"
+        sentMsg = await ctx.send(f"To unregister, react with {unregisterIcon}")
+        await sentMsg.add_reaction(unregisterIcon)
 
-        if code != hexcode:
-            df.warn(f"User {ctx.message.author.id} tried to unregister, but said the wrong hexcode.")
-            await ctx.send(f"Incorrect code. You said: `{code}`. The correct code was: `{hexcode}`. Try again.", delete_after = 10)
+        def check(reaction, user):
+            print(reaction)
+            print(user)
+            return reaction.message.id == sentMsg.id \
+                and user.id == ctx.message.author.id \
+                and str(reaction.emoji) == unregisterIcon
+
+        try:
+            reaction, user = await self.bot.wait_for("reaction_add", timeout=60.0, check=check)
+        except asyncio.TimeoutError:
             return
+        finally:
+            await sentMsg.delete()
 
         userdb.delete(ctx.message.author.id)
         await removeUserRole(ctx.message.author)
 
         df.warn(f"User {ctx.message.author.id} successfully unregistered.")
-        await ctx.send(f"Correct code! Unregistered {ctx.message.author.name}", delete_after = 5)
+        await ctx.send(f"Unregistered {ctx.message.author.name}", delete_after = 5)
 
     @commands.Cog.listener()
     async def on_message(self, m):
         await digisize.nickUpdate(m.author)
 
 
-# Necessary.
+# Necessary
 def setup(bot):
     bot.add_cog(RegisterCog(bot))
