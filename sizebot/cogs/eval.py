588d7bf537447a294cc74a3f899324abe08169dd
Natalie Fearnley <nfearnley@gmail.com>
2020-01-06 17:52:08 -0500

    Added eval progress indicators

--- sizebot/cogs/eval.py
+++ sizebot/cogs/eval.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 
 from sizebot import utils
@@ -14,24 +15,39 @@ class EvalCog(commands.Cog):
     @commands.check(requireAdmin)
     async def eval(self, ctx, *, evalStr):
         await logger.info(f"{ctx.message.author.display_name} tried to eval {evalStr!r}.")
-        try:
-            result = await runEval(ctx, evalStr)
-        except Exception as err:
-            await logger.error("eval error:\n" + utils.formatTraceback(err))
-            await ctx.send(f"> **ERROR:** `{str(err)!r}`")
-            return
+
+        # Show user that bot is busy doing something
+        waitMsg = None
+        if isinstance(ctx.channel, discord.TextChannel):
+            waitMsg = await ctx.send(f"<a:loading:663876493771800603>")
+
+        async with ctx.typing():
+            try:
+                result = await runEval(ctx, evalStr)
+            except Exception as err:
+                await logger.error("eval error:\n" + utils.formatTraceback(err))
+                await ctx.send(f"> **ERROR:** `{str(err)!r}`")
+                return
+            finally:
+                # Remove wait message when done
+                if waitMsg:
+                    await waitMsg.delete(delay=0)
+
         await utils.sendMessage(ctx, f"{result}")
 
     @commands.command()
     @commands.check(requireAdmin)
     async def evil(self, ctx, *, evalStr):
-        await ctx.message.delete(delay=0)
+        await ctx.message.delete(delay = 0)
+
         await logger.info(f"{ctx.message.author.display_name} tried to quietly eval {evalStr!r}.")
-        try:
-            await runEval(ctx, evalStr)
-        except Exception as err:
-            await logger.error("eval error:\n" + utils.formatTraceback(err))
-            await ctx.message.author.send(f"> **ERROR:** `{str(err)!r}`")
+
+        async with ctx.typing():
+            try:
+                await runEval(ctx, evalStr, returnValue = False)
+            except Exception as err:
+                await logger.error("eval error:\n" + utils.formatTraceback(err))
+                await ctx.message.author.send(f"> **ERROR:** `{str(err)!r}`")
 
 
 # Necessary
