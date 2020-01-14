3248403c4de4070d72554986bc8d310ee29fb167
Natalie Fearnley <nfearnley@gmail.com>
2020-01-13 19:55:01 -0500

    fixed unit type

--- sizebot/userdb.py
+++ sizebot/userdb.py
@@ -71,7 +71,7 @@ class User:
 
     @property
     def weight(self):
-        return self.baseweight * (self.viewscale ** Decimal("3"))
+        return WV(self.baseweight * (self.viewscale ** Decimal("3")))
 
     # Check that unitsystem is valid and lowercase
     @property
