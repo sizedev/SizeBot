bbc47dfdc772311ac9a091a24463d5b3cd1a766c
Natalie Fearnley <nfearnley@gmail.com>
2020-01-20 06:40:38 -0500

    Moved banned check into banned cog

--- sizebot/cogs/banned.py
+++ sizebot/cogs/banned.py
@@ -1,4 +1,13 @@
-from sizebot.lib.checks import denyGuildBan
+import discord
+
+
+def denyGuildBan(ctx):
+    member = ctx.author
+    isMember = isinstance(member, discord.Member)
+    if not isMember:
+        return True
+    isGuildBanned = discord.utils.get(member.roles, name = "SizeBot_Banned") is not None
+    return not isGuildBanned
 
 
 def setup(bot):
--- sizebot/lib/checks.py
+++ sizebot/lib/checks.py
@@ -10,12 +10,3 @@ def requireAdmin(ctx):
     if ctx.author.id not in conf.admins:
         raise commands.CommandInvokeError(errors.AdminPermissionException())
     return True
-
-
-def denyGuildBan(ctx):
-    member = ctx.author
-    isMember = isinstance(member, discord.Member)
-    if not isMember:
-        return True
-    isGuildBanned = discord.utils.get(member.roles, name = "SizeBot_Banned") is not None
-    return not isGuildBanned
