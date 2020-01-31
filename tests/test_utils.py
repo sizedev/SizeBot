a5c3ac85665fd73215d650dbbe26a9656ecc7091
DigiDuncan <digiduncan@gmail.com>
2020-01-31 03:53:05 -0500

    Made a few more tests for utils.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,3 +5,23 @@ def test_chunkLines_too_long():
     s = "".join(str(n) for (n) in range(10)) * 10
     result = list(utils.chunkLines(s, 21))
     assert result == ["012345678901234567890", "123456789012345678901", "234567890123456789012", "345678901234567890123", "4567890123456789"]
+
+
+def test_removeBrackets_square():
+    s = "[value]"
+    result = utils.removeBrackets(s)
+    assert result == "value"
+
+
+def test_removeBrackets_pointy():
+    s = "<value>"
+    result = utils.removeBrackets(s)
+    assert result == "value"
+
+
+def test_clamp_up():
+    assert utils.clamp(10, 1, 20) == 10
+
+
+def test_clamp_down():
+    assert utils.clamp(10, 100, 20) == 20
