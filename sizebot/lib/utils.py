a31e2784de4bbd044d76c6545d6760d5df85241b
DigiDuncan <digiduncan@gmail.com>
2020-02-02 16:26:00 -0500

    Added a random int to Roman method I found on the internet.

--- sizebot/lib/utils.py
+++ sizebot/lib/utils.py
@@ -281,3 +281,20 @@ def stripSizeTag(s):
         s_sizetagloose = re.sub(re_sizetagloose, r"\1", s)
         return s_sizetagloose
     return s
+
+
+def intToRoman(input):
+    """ Convert an integer to a Roman numeral. """
+
+    if not isinstance(input, type(1)):
+        raise TypeError("expected integer, got %s" % type(input))
+    if not 0 < input < 4000:
+        raise ValueError("Argument must be between 1 and 3999")
+    ints = (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+    nums = ('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
+    result = []
+    for i in range(len(ints)):
+        count = int(input / ints[i])
+        result.append(nums[i] * count)
+        input -= ints[i] * count
+    return ''.join(result)
