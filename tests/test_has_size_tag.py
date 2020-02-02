66823df6db0da7ddeab840143d5b814d451dde19
DigiDuncan <digiduncan@gmail.com>
2020-02-02 13:17:14 -0500

    Added tests for tags with species.

--- tests/test_has_size_tag.py
+++ tests/test_has_size_tag.py
@@ -189,3 +189,193 @@ def test_three_units():
 def test_too_long_unit():
     val = hasSizeTag("DigiDuncan [50nuggets]")
     assert val is False
+
+
+def test_integer_one_unit_plus_species():
+    val = hasSizeTag("DigiDuncan [1m, Species]")
+    assert val is True
+
+
+def test_integer_one_unit_plus_commas_plus_species():
+    val = hasSizeTag("DigiDuncan [1,000mi, Species]")
+    assert val is True
+
+
+def test_integer_one_long_unit_plus_species():
+    val = hasSizeTag("DigiDuncan [1kuni, Species]")
+    assert val is True
+
+
+def test_integer_one_long_unit_plus_commas_plus_species():
+    val = hasSizeTag("DigiDuncan [1,000Yuni, Species]")
+    assert val is True
+
+
+def test_integer_feet_no_inches_plus_species():
+    val = hasSizeTag("DigiDuncan [5', Species]")
+    assert val is True
+
+
+def test_integer_feet_no_inches_plus_commas_plus_species():
+    val = hasSizeTag("DigiDuncan [5,000', Species]")
+    assert val is True
+
+
+def test_integer_inches_plus_species():
+    val = hasSizeTag("DigiDuncan [8\", Species]")
+    assert val is True
+
+
+def test_integer_feet_and_inches_plus_species():
+    val = hasSizeTag("DigiDuncan [5'8\", Species]")
+    assert val is True
+
+
+def test_integer_feet_and_inches_plus_commas_plus_species():
+    val = hasSizeTag("DigiDuncan [5,000'8\", Species]")
+    assert val is True
+
+
+def test_integer_feet_and_fractional_inches_plus_species():
+    val = hasSizeTag("DigiDuncan [5'8½\", Species]")
+    assert val is True
+
+
+def test_integer_feet_and_fractional_inches_plus_commas_plus_species():
+    val = hasSizeTag("DigiDuncan [5,000'8½\", Species]")
+    assert val is True
+
+
+def test_decimal_feet_plus_species():
+    val = hasSizeTag("DigiDuncan [5.8', Species]")
+    assert val is True
+
+
+def test_decimal_inches_plus_species():
+    val = hasSizeTag("DigiDuncan [8.5\", Species]")
+    assert val is True
+
+
+def test_decimal_feet_and_inches_plus_species():
+    val = hasSizeTag("DigiDuncan [5'8.5\", Species]")
+    assert val is True
+
+
+def test_decimal_one_unit_plus_species():
+    val = hasSizeTag("DigiDuncan [1.1m, Species]")
+    assert val is True
+
+
+def test_decimal_one_unit_plus_commas_plus_species():
+    val = hasSizeTag("DigiDuncan [1,000mi, Species]")
+    assert val is True
+
+
+def test_decimal_one_long_unit_plus_species():
+    val = hasSizeTag("DigiDuncan [1.1kuni, Species]")
+    assert val is True
+
+
+def test_decimal_one_long_unit_plus_commas_plus_species():
+    val = hasSizeTag("DigiDuncan [1,000.1Yuni, Species]")
+    assert val is True
+
+
+def test_integer_E_pos_no_plus_plus_species():
+    val = hasSizeTag("DigiDuncan [1E10ly, Species]")
+    assert val is True
+
+
+def test_integer_E_pos_plus_plus_species():
+    val = hasSizeTag("DigiDuncan [1E+10ly, Species]")
+    assert val is True
+
+
+def test_integer_E_neg_plus_species():
+    val = hasSizeTag("DigiDuncan [1E-10ly, Species]")
+    assert val is True
+
+
+def test_integer_e_pos_no_plus_plus_species():
+    val = hasSizeTag("DigiDuncan [1e10ly, Species]")
+    assert val is True
+
+
+def test_integer_e_pos_plus_plus_species():
+    val = hasSizeTag("DigiDuncan [1e+10ly, Species]")
+    assert val is True
+
+
+def test_integer_e_neg_plus_species():
+    val = hasSizeTag("DigiDuncan [1e-10ly, Species]")
+    assert val is True
+
+
+def test_decimal_E_pos_no_plus_plus_species():
+    val = hasSizeTag("DigiDuncan [1.1E10ly, Species]")
+    assert val is True
+
+
+def test_decimal_E_pos_plus_plus_species():
+    val = hasSizeTag("DigiDuncan [1.1E+10ly, Species]")
+    assert val is True
+
+
+def test_decimal_E_neg_plus_species():
+    val = hasSizeTag("DigiDuncan [1.1E-10ly, Species]")
+    assert val is True
+
+
+def test_decimal_e_pos_no_plus_plus_species():
+    val = hasSizeTag("DigiDuncan [1.1e10ly, Species]")
+    assert val is True
+
+
+def test_decimal_e_pos_plus_plus_species():
+    val = hasSizeTag("DigiDuncan [1.1e+10ly, Species]")
+    assert val is True
+
+
+def test_decimal_e_neg_plus_species():
+    val = hasSizeTag("DigiDuncan [1.1e-10ly, Species]")
+    assert val is True
+
+
+def test_fractional_inch_plus_species():
+    val = hasSizeTag("DigiDuncan [⅝in, Species]")
+    assert val is True
+
+
+def test_zero_plus_species():
+    val = hasSizeTag("DigiDuncan [0, Species]")
+    assert val is True
+
+
+def test_infinity_plus_species():
+    val = hasSizeTag("DigiDuncan [∞, Species]")
+    assert val is True
+
+
+def test_zalgo_plus_species():
+    val = hasSizeTag("D̶̨i͏̢͟g͞i̴͡D͝ư͢nc͞an̸ [5'8\", Species]")
+    assert val is True
+
+
+def test_brackets_in_name_plus_species():
+    val = hasSizeTag("[DigiDuncan] [5'8\", Species]")
+    assert val is True
+
+
+def test_three_units_plus_species():
+    val = hasSizeTag("DigiDuncan [5km5m5cm, Species]")
+    assert val is False
+
+
+def test_too_long_unit_plus_species():
+    val = hasSizeTag("DigiDuncan [50nuggets, Species]")
+    assert val is False
+
+
+def no_tag():
+    val = hasSizeTag("DigiDuncan")
+    assert val is False
