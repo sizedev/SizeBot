77f00325e5930b4b54eacf439258fd8286c6704b
DigiDuncan <digiduncan@gmail.com>
2020-02-02 15:19:57 -0500

    Start the holiday cog.

---
+++ sizebot/cogs/holiday.py
@@ -0,0 +1,25 @@
+import logging
+
+from discord.ext import commands, tasks
+
+logger = logging.getLogger("sizebot")
+
+
+class HolidayCog(commands.Cog):
+    """Do stuff on holidays."""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.holidayTask.start()
+
+    def cog_unload(self):
+        self.holidayTask.cancel()
+
+    @tasks.loop(seconds=60)
+    async def holidayTask(self):
+        """Holiday checker"""
+        pass
+
+
+def setup(bot):
+    bot.add_cog(HolidayCog(bot))
--- sizebot/main.py
+++ sizebot/main.py
@@ -24,6 +24,7 @@ initial_cogs = [
     "eval",
     "fun",
     "help",
+    "holiday",
     "keypad",
     "meicros",
     "monika",
