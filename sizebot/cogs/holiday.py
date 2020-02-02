f6181deae1bea5ecd1473358a9fe1a243d37cc96
DigiDuncan <digiduncan@gmail.com>
2020-02-02 16:27:17 -0500

    Started adding special names.

--- sizebot/cogs/holiday.py
+++ sizebot/cogs/holiday.py
@@ -1,6 +1,10 @@
 import logging
 
 from discord.ext import commands, tasks
+from datetime import datetime, time, date
+from sizebot import conf
+
+from utils import intToRoman
 
 logger = logging.getLogger("sizebot")
 
@@ -18,7 +22,22 @@ class HolidayCog(commands.Cog):
     @tasks.loop(seconds=60)
     async def holidayTask(self):
         """Holiday checker"""
-        pass
+        now = datetime.now()
+        nowtime = now.time()
+        nowday = now.day()
+        midnight = time(hour = 0, minute = 0)
+        # Make sure our loop point is midnight.
+        if nowtime != midnight:
+            timeuntilmidnight = midnight - nowtime
+            self.holidayTask.change_interval(seconds = timeuntilmidnight.total_seconds())
+        # Holiday checks.
+        newnick = conf.name
+        if nowday == date(month = 1, day = 1).day():  # New Year's Day
+            newnick += f" {intToRoman(int(now.year))}"
+        if nowday == date(month = 3, day = 10).day():  # Digi's birthday
+            newnick += " 🎉"
+        if newnick != self.bot.user.nickname:
+            self.bot.user.edit(username = newnick)
 
 
 def setup(bot):
