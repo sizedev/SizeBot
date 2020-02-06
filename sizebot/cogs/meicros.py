7cf2efbb7b51f0a6a45beae6f8ba8cb6b3b670f3
DigiDuncan <digiduncan@gmail.com>
2020-02-06 05:44:57 -0500

    Fixed Meicros.

--- sizebot/cogs/meicros.py
+++ sizebot/cogs/meicros.py
@@ -25,8 +25,17 @@ class MeicrosCog(commands.Cog):
             return
         # Emulate what a good stop command would be.
         if m.content.startswith("!stop"):
-            # TODO: Need to join voice channel first
+            channel = m.author.voice.channel
+            await channel.connect()
             await m.channel.send("!play stop", delete_after = 0)
+            leave = m.guild.voice_client
+            await leave.disconnect()
+        if m.content.startswith("!queue"):
+            channel = m.author.voice.channel
+            await channel.connect()
+            await m.channel.send("!play queue", delete_after = 0)
+            leave = m.guild.voice_client
+            await leave.disconnect()
         # Tell our good friend how they can fund SizeBot.
         if m.author.id == chocola and isACommand(self.bot.all_commands.keys(), m.content) and not m.content.endswith("--no-fund"):
             await m.channel.send(f"1 packages are looking funding.\nRun `{conf.prefix}fund` for details.\n(To stop seeing this message, run commands with `--no-fund`.)")
