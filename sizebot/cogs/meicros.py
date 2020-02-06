26261240a2881bd7b6648dab99d01f8f13127fd9
DigiDuncan <digiduncan@gmail.com>
2020-02-06 14:15:26 -0500

    Way more Meicros.

--- sizebot/cogs/meicros.py
+++ sizebot/cogs/meicros.py
@@ -23,19 +23,39 @@ class MeicrosCog(commands.Cog):
     async def on_message(self, m):
         if m.author.bot:
             return
-        # Emulate what a good stop command would be.
+
+        # !play macros.
         if m.content.startswith("!stop"):
             channel = m.author.voice.channel
             await channel.connect()
             await m.channel.send("!play stop", delete_after = 0)
             leave = m.guild.voice_client
             await leave.disconnect()
-        if m.content.startswith("!queue"):
+        elif m.content.startswith("!pause"):
+            channel = m.author.voice.channel
+            await channel.connect()
+            await m.channel.send("!play pause", delete_after = 0)
+            leave = m.guild.voice_client
+            await leave.disconnect()
+        elif m.content.startswith("!resume"):
+            channel = m.author.voice.channel
+            await channel.connect()
+            await m.channel.send("!play resume", delete_after = 0)
+            leave = m.guild.voice_client
+            await leave.disconnect()
+        elif m.content.startswith("!queue"):
             channel = m.author.voice.channel
             await channel.connect()
             await m.channel.send("!play queue", delete_after = 0)
             leave = m.guild.voice_client
             await leave.disconnect()
+        elif m.content.startswith("!current"):
+            channel = m.author.voice.channel
+            await channel.connect()
+            await m.channel.send("!play current", delete_after = 0)
+            leave = m.guild.voice_client
+            await leave.disconnect()
+
         # Tell our good friend how they can fund SizeBot.
         if m.author.id == chocola and isACommand(self.bot.all_commands.keys(), m.content) and not m.content.endswith("--no-fund"):
             await m.channel.send(f"1 packages are looking funding.\nRun `{conf.prefix}fund` for details.\n(To stop seeing this message, run commands with `--no-fund`.)")
