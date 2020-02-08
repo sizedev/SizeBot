98a6e89b52a59db61b22173da5cb9887f55dfa38
DigiDuncan <digiduncan@gmail.com>
2020-02-07 20:50:19 -0500

    Fix for prettyTimeDelta.

--- sizebot/lib/utils.py
+++ sizebot/lib/utils.py
@@ -49,28 +49,30 @@ def clamp(minVal, val, maxVal):
 
 
 def prettyTimeDelta(totalSeconds, millisecondAccuracy = False):
-    SECONDS_PER_YEAR = 86400 * 365
-    SECONDS_PER_DAY = 86400
-    SECONDS_PER_HOUR = 3600
-    SECONDS_PER_MINUTE = 60
+    MILLISECONDS_PER_YEAR = 86400 * 365 * 1000
+    MILLISECONDS_PER_DAY = 86400 * 1000
+    MILLISECONDS_PER_HOUR = 3600 * 1000
+    MILLISECONDS_PER_MINUTE = 60 * 1000
+    MILLISECONDS_PER_SECOND = 1000
 
-    seconds = int(totalSeconds)
-    years, seconds = divmod(seconds, SECONDS_PER_YEAR)
-    days, seconds = divmod(seconds, SECONDS_PER_DAY)
-    hours, seconds = divmod(seconds, SECONDS_PER_HOUR)
-    minutes, seconds = divmod(seconds, SECONDS_PER_MINUTE)
+    milliseconds = int(totalSeconds * 1000)
+    years, milliseconds = divmod(milliseconds, MILLISECONDS_PER_YEAR)
+    days, milliseconds = divmod(milliseconds, MILLISECONDS_PER_DAY)
+    hours, milliseconds = divmod(milliseconds, MILLISECONDS_PER_HOUR)
+    minutes, milliseconds = divmod(milliseconds, MILLISECONDS_PER_MINUTE)
+    seconds, milliseconds = divmod(milliseconds, MILLISECONDS_PER_SECOND)
 
     s = ""
-    if totalSeconds >= SECONDS_PER_YEAR:
+    if totalSeconds >= MILLISECONDS_PER_YEAR:
         s += f"{years:d} years, "
-    if totalSeconds >= SECONDS_PER_DAY:
+    if totalSeconds >= MILLISECONDS_PER_DAY:
         s += f"{days:d} days, "
-    if totalSeconds >= SECONDS_PER_HOUR:
+    if totalSeconds >= MILLISECONDS_PER_HOUR:
         s += f"{hours:d} hours, "
-    if totalSeconds >= SECONDS_PER_MINUTE:
+    if totalSeconds >= MILLISECONDS_PER_MINUTE:
         s += f"{minutes:d} minutes, "
     if millisecondAccuracy:
-        s += f"{seconds:.3} seconds"
+        s += f"{seconds:d}.{milliseconds:d} seconds"
     else:
         s += f"{seconds:d} seconds"
 
