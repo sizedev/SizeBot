e70e723b529880f1f1af004ddde1dda8ab08206b
DigiDuncan <digiduncan@gmail.com>
2020-03-17 16:00:12 -0400

    Rainbow role? OwO

---
+++ sizebot/cogs/rainbow.py
@@ -0,0 +1,22 @@
+from discord.ext import tasks, commands
+
+colorlist = [0xff0000, 0xff7700, 0xffff00, 0x00ff00, 0x00ffff, 0x0000ff, 0xff00ff]
+
+
+class MyCog(commands.Cog):
+    def __init__(self, bot):
+        self.index = 0
+        self.rainbower.start()
+
+    def cog_unload(self):
+        self.rainbower.cancel()
+
+    @tasks.loop(seconds=1.0)
+    async def rainbower(self):
+        self.index += 1
+        if self.index >= 7:
+            self.index == 0
+        for guild in self.bot.guilds:
+            for role in guild.roles:
+                if role.name.lower() == "Rainbow":
+                    await role.edit(color = colorlist[self.index])
