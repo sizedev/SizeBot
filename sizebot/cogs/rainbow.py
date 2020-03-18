0c5aec85d7bddabecf02dae5c545efca9e949650
DigiDuncan <digiduncan@gmail.com>
2020-03-18 00:41:49 -0400

    Switch to an generator.

--- sizebot/cogs/rainbow.py
+++ sizebot/cogs/rainbow.py
@@ -1,10 +1,11 @@
 import logging
+import itertools
 
 from discord.ext import tasks, commands
 
 from sizebot.lib.utils import formatTraceback
 
-colorlist = [0xff0000, 0xff7700, 0xffff00, 0x00ff00, 0x00ffff, 0x0000ff, 0xff00ff]
+colorgen = itertools.cycle([0xff0000, 0xff7700, 0xffff00, 0x00ff00, 0x00ffff, 0x0000ff, 0xff00ff])
 
 logger = logging.getLogger("sizebot")
 
@@ -12,7 +13,6 @@ logger = logging.getLogger("sizebot")
 class RainbowCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.index = 0
         self.rainbower.start()
 
     def cog_unload(self):
@@ -21,13 +21,10 @@ class RainbowCog(commands.Cog):
     @tasks.loop(seconds=1.0)
     async def rainbower(self):
         try:
-            self.index += 1
-            if self.index >= len(colorlist):
-                self.index = 0
             for guild in self.bot.guilds:
                 for role in guild.roles:
                     if role.name.lower() == "rainbow":
-                        await role.edit(color = colorlist[self.index])
+                        await role.edit(color = next(colorgen))
         except Exception as err:
             logger.error(formatTraceback(err))
 
