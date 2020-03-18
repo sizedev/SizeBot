5dc27b63dbec59bf96cee145680687367d992d33
DigiDuncan <digiduncan@gmail.com>
2020-03-18 00:32:58 -0400

    Log errors for the Rainbower.

--- sizebot/cogs/rainbow.py
+++ sizebot/cogs/rainbow.py
@@ -1,7 +1,12 @@
+import logging
+
 from discord.ext import tasks, commands
+
+from sizebot.lib.utils import formatTraceback
 
 colorlist = [0xff0000, 0xff7700, 0xffff00, 0x00ff00, 0x00ffff, 0x0000ff, 0xff00ff]
 
+logger = logging.getLogger("sizebot")
 
 class RainbowCog(commands.Cog):
     def __init__(self, bot):
@@ -14,13 +19,21 @@ class RainbowCog(commands.Cog):
 
     @tasks.loop(seconds=1.0)
     async def rainbower(self):
-        self.index += 1
-        if self.index >= 7:
-            self.index == 0
-        for guild in self.bot.guilds:
-            for role in guild.roles:
-                if role.name.lower() == "rainbow":
-                    await role.edit(color = colorlist[self.index])
+        try:
+            self.index += 1
+            if self.index >= 7:
+                self.index == 0
+            for guild in self.bot.guilds:
+                for role in guild.roles:
+                    if role.name.lower() == "rainbow":
+                        await role.edit(color = colorlist[self.index])
+        except Exception as err:
+            logger.error(formatTraceback(err))
+
+    @rainbower.before_loop
+    async def before_rainbower(self):
+        await self.bot.wait_until_ready()
+
 
 
 def setup(bot):
