fa95aa1c12d9c571aef094733e121a44fe3339f6
DigiDuncan <digiduncan@gmail.com>
2020-03-19 06:28:21 -0400

    Explain myself.

--- sizebot/plugins/edge.py
+++ sizebot/plugins/edge.py
@@ -1,3 +1,7 @@
+# The "edge" extensions allows bot admins (and later guild owners [SB4]) to set a largest/smallest user (for their server [SB4]).
+# It does this by sseeing if they are the largest or smallest user (in the guild [SB4]), and if they aren't setting their height
+# to either 1.1x or 0.9x of the largest or smallest user (in the guild [SB4]), respectively.
+
 import toml
 from sizebot import conf
 
