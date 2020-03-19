21b5e98f1102be9aca72457999b9fab8f956b7dc
DigiDuncan <digiduncan@gmail.com>
2020-03-19 05:40:52 -0400

    Load the edge users list in.

--- sizebot/plugins/edge.py
+++ sizebot/plugins/edge.py
@@ -1,6 +1,14 @@
 import toml
 from sizebot import conf
 
+try:
+    with open(conf.edgepath, "r") as f:
+        edgedict = toml.loads(f.read())
+except (FileNotFoundError, TypeError, toml.TomlDecodeError):
+    edgedict = {"edges": {"smallest": None, "largest": None}}
+    with open(conf.edgepath, "w") as f:
+        f.write(toml.dump(edgedict))
+
 
 async def on_message(m):
     pass
