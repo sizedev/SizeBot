0d06099442acc50c98bc8495c9c9793802561015
DigiDuncan <digiduncan@gmail.com>
2020-03-19 07:14:48 -0400

    Probably fix the help message.

--- sizebot/cogs/help.py
+++ sizebot/cogs/help.py
@@ -97,7 +97,7 @@ class HelpCog(commands.Cog):
         if cmd.help:
             descriptionParts.append(cmd.help)
         description = ""
-        if ctx.bot.is_owner() in cmd.checks:
+        if "is_owner" in repr(cmd.checks):
             description += ":rotating_light: **THIS COMMAND IS FOR BOT OWNERS ONLY** :rotating_light:\n"
         description += "\n\n".join(descriptionParts)
 
