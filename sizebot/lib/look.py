0020543f50759bcc8cf1a4612112a41cb03814fd
DigiDuncan <digiduncan@gmail.com>
2020-03-30 05:23:42 -0400

    Finish the basics of ObjectComparison.

--- sizebot/lib/look.py
+++ sizebot/lib/look.py
@@ -1,20 +1,33 @@
 # import discord
 
 # from sizebot import __version__
+from sizebot.lib.constants import emojis
 from sizebot.lib.units import SV, WV
 
 
 class ObjectComparison:
     def __init__(self, userdata, obj):
         self.multiplier = self.userdata.viewscale
+        self.obj = obj
 
-        self.perceivedlength = obj.length and SV(obj.length * self.multiplier)
-        self.perceivedheight = obj.height and SV(obj.height * self.multiplier)
-        self.perceivedwidth = obj.width and SV(obj.width * self.multiplier)
-        self.perceiveddepth = obj.depth and SV(obj.depth * self.multiplier)
-        self.perceivedweight = obj.weight and WV(obj.weight * (self.multiplier ** 3))
+        self.perceivedlength = self.obj.length and SV(self.obj.length * self.multiplier)
+        self.perceivedheight = self.obj.height and SV(self.obj.height * self.multiplier)
+        self.perceivedwidth = self.obj.width and SV(self.obj.width * self.multiplier)
+        self.perceiveddepth = self.obj.depth and SV(self.obj.depth * self.multiplier)
+        self.perceivedweight = self.obj.weight and WV(self.obj.weight * (self.multiplier ** 3))
 
     def __str__(self):
         returnstr = f"{self.userdata.nickname} is {self.userdata.height:,.3mu} tall.\n"
-        returnstr += f"To {self.userdata.nickname}..."
+        returnstr += f"To {self.userdata.nickname}, {self.obj.article} {self.obj.name} looks...\n"
+        if not self.perceivedheight:
+            returnstr += f"{emojis.blank}{self.perceivedlength:,.3mu} tall\n"
+        if self.perceivedheight:
+            returnstr += f"{emojis.blank}{self.perceivedheight:,.3mu} tall\n"
+        if self.perceivedwidth:
+            returnstr += f"{emojis.blank}{self.perceivedwidth:,.3mu} wide\n"
+        if self.perceiveddepth:
+            returnstr += f"{emojis.blank}{self.perceiveddepth:,.3mu} deep\n"
+        if self.perceivedwidth:
+            returnstr += "and weighs"
+            returnstr += f"{emojis.blank}{self.perceivedweight:,.3mu}"
         return returnstr
