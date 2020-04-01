dc18c4b15d2955ba1fc6e24f1f41eb0132db9811
DigiDuncan <digiduncan@gmail.com>
2020-04-01 07:47:13 -0400

    Add aliases to &help.

--- sizebot/cogs/help.py
+++ sizebot/cogs/help.py
@@ -75,7 +75,7 @@ class HelpCog(commands.Cog):
 
         commandsPerPage = math.ceil(len(commands) / (embedCount * pagesPerEmbed))
 
-        commandStrings = [f"**{c.name}**\n{c.short_doc or '-'}" for c in commands]
+        commandStrings = [f"**{c.name}** *({", ".join(cmd.aliases)})*\n{c.short_doc or '-'}" for c in commands]
 
         pages = ["\n".join(p) for p in utils.chunkList(commandStrings, commandsPerPage)]
 
