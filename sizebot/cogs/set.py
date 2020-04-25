74b6c235e709cf88c952ee9acbfc323232b564ef
DigiDuncan <digiduncan@gmail.com>
2020-04-25 02:25:18 -0400

    Make verbage more clear in reset commands.

--- sizebot/cogs/set.py
+++ sizebot/cogs/set.py
@@ -309,7 +309,7 @@ class SetCog(commands.Cog):
         userdb.save(userdata)
 
         logger.info(f"User {ctx.author.id} ({ctx.author.display_name}) removed their custom hair length.")
-        await ctx.send(f"<@{ctx.author.id}>'s hair length is now default.")
+        await ctx.send(f"<@{ctx.author.id}>'s hair length is now cleared.")
 
     @commandsplus.command(
         usage = "<length>"
@@ -339,7 +339,7 @@ class SetCog(commands.Cog):
         userdb.save(userdata)
 
         logger.info(f"User {ctx.author.id} ({ctx.author.display_name}) removed their custom tail length.")
-        await ctx.send(f"<@{ctx.author.id}>'s tail length is now default.")
+        await ctx.send(f"<@{ctx.author.id}>'s tail length is now cleared.")
 
     @commandsplus.command(
         usage = "<male/female/none>"
