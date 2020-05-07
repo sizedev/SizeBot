7852769a91babdc910fcc5d44c5b70420a6288f5
DigiDuncan <digiduncan@gmail.com>
2020-05-07 03:10:27 -0400

    Or not!

--- sizebot/cogs/help.py
+++ sizebot/cogs/help.py
@@ -110,8 +110,7 @@ class HelpCog(commands.Cog):
             if not cat_cmds:
                 logger.warn(f"Command category {cat.cid!r} is empty.")
                 continue
-            category_text = f"__{cat.description}__\n"
-            category_text += "\n".join([c.short_doc for c in cat_cmds])
+            category_text = "\n".join([c.short_doc for c in cat_cmds])
             embed.add_field(name=cat.name, value=category_text, inline=True)
             if n % 2 == 1:
                 embed.add_field(name="", inline=False)
