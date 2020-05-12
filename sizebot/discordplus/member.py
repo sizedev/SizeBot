32c18982c9755952963005cfa4c75a871a44c039
DigiDuncan <digiduncan@gmail.com>
2020-05-11 23:02:58 -0400

    Bare bones soft gender/pronoun detector.

--- sizebot/discordplus/member.py
+++ sizebot/discordplus/member.py
@@ -1,14 +1,79 @@
 from discord import Member
 
+genderrolemap = {
+    "male":   "m",
+    "boy":    "m",
+    "man":    "m",
+    "female": "f",
+    "girl":   "f",
+    "woman":  "f"
+}
+
+pronounrolemap = {
+    "he/his":    "he",
+    "she/her":   "she",
+    "they/them": "they"
+}
+
+
+@property
+def rolelist(self):
+    return [role.name for role in self.roles]
+
 
 @property
 def gender(self):
-    return None
+    foundmale = False
+    foundfemale = False
+
+    for role in self.rolelist:
+        if genderrolemap[role] == "m":
+            foundmale = True
+        elif genderrolemap[role] == "f":
+            foundfemale = True
+
+    if not foundmale and not foundfemale:
+        if self.pronoun == "he":
+            foundmale = True
+        if self.pronoun == "she":
+            foundfemale = True
+
+    if foundmale ^ foundfemale:
+        return None
+    if foundmale:
+        return "m"
+    if foundfemale:
+        return "f"
 
 
 @property
 def pronoun(self):
-    return None
+    foundhe = False
+    foundshe = False
+    foundthey = False
+
+    for role in self.rolelist:
+        if pronounrolemap[role] == "he":
+            foundhe = True
+        elif pronounrolemap[role] == "she":
+            foundshe = True
+        elif pronounrolemap[role] == "they":
+            foundthey = True
+
+    if not foundhe and not foundshe and not foundthey:
+        if self.gender == "m":
+            foundhe = True
+        if self.gender == "f":
+            foundshe = True
+
+    if foundhe ^ foundshe ^ foundthey:
+        return None
+    if foundhe:
+        return "he"
+    if foundshe:
+        return "she"
+    if foundthey:
+        return "they"
 
 
 def patch():
