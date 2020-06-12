ce2894875b5685e32b185b773b88b51bb6369c95
DigiDuncan <digiduncan@gmail.com>
2020-06-11 21:38:51 -0400

    Fix Tupperbox catcher.

--- sizebot/extensions/tupperbox.py
+++ sizebot/extensions/tupperbox.py
@@ -1,6 +1,9 @@
+import conf
+
+
 # Ignore Tupperboxes being mistaken for commands.
 def ignoreTupperbox(ctx):
-    return not (ctx.message.content.startswith("&") and ctx.message.content.endswith("&"))
+    return not (ctx.message.content.startswith(conf.prefix) and ctx.message.content.endswith(conf.prefix))
 
 
 def setup(bot):
