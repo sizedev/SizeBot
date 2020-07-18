2d46f26d0d00250181373c71780acda5111b4e95
DigiDuncan <digiduncan@gmail.com>
2020-07-17 23:20:12 -0400

    Fix tests.

--- tests/test_macrovision.py
+++ tests/test_macrovision.py
@@ -8,29 +8,13 @@ asyncio.run(units.init())
 
 def test_macrovision():
     assert macrovision.get_url([
-        {
-            "name": "Duncan",
-            "model": "man1",
-            "height": Decimal("0.0127")
-        },
-        {
-            "name": "Natalie",
-            "model": "woman1",
-            "height": Decimal("0.1524")
-        }
+        ("Duncan", "man1", Decimal("0.0127")),
+        ("Natalie", "woman1", Decimal("0.1524"))
     ]) == "https://macrovision.crux.sexy/?scene=eyJlbnRpdGllcyI6IFt7Im5hbWUiOiAiSHVtYW4iLCAiY3VzdG9tTmFtZSI6ICJOYXRhbGllIiwgInNjYWxlIjogMC4wODk1NTIyMzg4MDU5NzAxNCwgInZpZXciOiAid29tYW4xIiwgIngiOiAiMCIsICJ5IjogIjAiLCAicHJpb3JpdHkiOiAwLCAiYnJpZ2h0bmVzcyI6IDF9LCB7Im5hbWUiOiAiSHVtYW4iLCAiY3VzdG9tTmFtZSI6ICJEdW5jYW4iLCAic2NhbGUiOiAwLjAwNzA0MjI1MzUyMTEyNjc2MSwgInZpZXciOiAibWFuMSIsICJ4IjogIjAuMDM4MSIsICJ5IjogIjAiLCAicHJpb3JpdHkiOiAwLCAiYnJpZ2h0bmVzcyI6IDF9XSwgIndvcmxkIjogeyJoZWlnaHQiOiAwLjE1MjQsICJ1bml0IjogIm1ldGVycyIsICJ4IjogIjAiLCAieSI6ICIwIn0sICJ2ZXJzaW9uIjogM30="
 
 
 def test_macrovision_SV():
     assert macrovision.get_url([
-        {
-            "name": "Duncan",
-            "model": "man1",
-            "height": SV.parse("0.5in")
-        },
-        {
-            "name": "Natalie",
-            "model": "woman1",
-            "height": SV.parse("6in")
-        }
+        ("Duncan", "man1", SV.parse("0.5in")),
+        ("Natalie", "woman1", SV.parse("6in"))
     ]) == "https://macrovision.crux.sexy/?scene=eyJlbnRpdGllcyI6IFt7Im5hbWUiOiAiSHVtYW4iLCAiY3VzdG9tTmFtZSI6ICJOYXRhbGllIiwgInNjYWxlIjogMC4wODk1NTIyMzg4MDU5NzAxNCwgInZpZXciOiAid29tYW4xIiwgIngiOiAiMCIsICJ5IjogIjAiLCAicHJpb3JpdHkiOiAwLCAiYnJpZ2h0bmVzcyI6IDF9LCB7Im5hbWUiOiAiSHVtYW4iLCAiY3VzdG9tTmFtZSI6ICJEdW5jYW4iLCAic2NhbGUiOiAwLjAwNzA0MjI1MzUyMTEyNjc2MSwgInZpZXciOiAibWFuMSIsICJ4IjogIjAuMDM4MSIsICJ5IjogIjAiLCAicHJpb3JpdHkiOiAwLCAiYnJpZ2h0bmVzcyI6IDF9XSwgIndvcmxkIjogeyJoZWlnaHQiOiAwLjE1MjQsICJ1bml0IjogIm1ldGVycyIsICJ4IjogIjAiLCAieSI6ICIwIn0sICJ2ZXJzaW9uIjogM30="
