0d6ed02fd4d23926d9107a4eec1dfbfdc922fda6
Natalie Fearnley <nfearnley@gmail.com>
2020-08-31 23:55:45 -0400

    Added api.py

---
+++ sizebot/lib/api.py
@@ -0,0 +1,10 @@
+from sizebot.lib import userdb
+from sizebot.lib.errors import UserNotFoundException
+
+
+def getSizebotUser(guildid, userid):
+    try:
+        u = userdb.load()
+    except UserNotFoundException:
+        return None
+    return u.nickname
