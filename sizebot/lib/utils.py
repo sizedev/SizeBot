7451be555093a2c4918a0238e90800c2542e4728
DigiDuncan <digiduncan@gmail.com>
2020-09-08 02:09:09 -0400

    Create regexbuild()

--- sizebot/lib/utils.py
+++ sizebot/lib/utils.py
@@ -371,3 +371,26 @@ def removesuffix(self: str, suffix: str, /) -> str:
         return self[:-len(suffix)]
     else:
         return self[:]
+
+
+def regexbuild(li: list, capture = False) -> str:
+    """
+    regexbuild(["a", "b", "c"])
+    >>> "a|b|c"
+    regexbuild(["a", "b", "c"], capture = True)
+    >>> "(a|b|c)"
+    regexbuild([["a", "b", "c"], ["x", "y", "zzz"]])
+    >>> "zzz|a|b|c|x|y"
+    """
+    escaped = []
+    for i in li:
+        if isinstance(i, list):
+            for ii in i:
+                escaped.append(re.escape(ii))
+        else:
+            escaped.append(re.escape(i))
+    li.sort(reverse = True)
+    returnstring = "|".join(li)
+    if capture:
+        returnstring = f"({returnstring})"
+    return returnstring
