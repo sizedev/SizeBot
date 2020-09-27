f9d5d2a6d9306bb77e1de64095efe717071a9c5e
DigiDuncan <digiduncan@gmail.com>
2020-09-26 20:11:43 -0400

    Finish guilddb

--- sizebot/lib/guilddb.py
+++ sizebot/lib/guilddb.py
@@ -1,27 +1,58 @@
 import json
-from copy import copy
 from functools import total_ordering
 
 from sizebot import conf
 from sizebot.lib import errors
+from sizebot.lib.units import SV
 
 
 @total_ordering
 class Guild:
     # __slots__ declares to python what attributes to expect.
-    __slots__ = ["id"]
+    __slots__ = ["id", "small_edge", "large_edge", "_high_limit", "_low_limit"]
 
     def __init__(self):
         self.id = None
+        self.small_edge = None
+        self.large_edge = None
+        self._high_limit = None
+        self._low_limit = None
+
+    @property
+    def high_limit(self):
+        return self._high_limit
+
+    @high_limit.setter
+    def high_limit(self, value):
+        self._high_limit = SV(max(0, SV(value)))
+
+    @property
+    def low_limit(self):
+        return self._low_limit
+
+    @low_limit.setter
+    def low_limit(self, value):
+        self._low_limit = SV(max(0, SV(value)))
 
     # Return an python dictionary for json exporting
     def toJSON(self):
-        return {}
+        return {
+            "id":         self.id,
+            "small_edge": self.small_edge,
+            "large_edge": self.large_edge,
+            "high_limit": None if self.high_limit is None else str(self.high_limit),
+            "low_limit":  None if self.low_limit is None else str(self.low_limit)
+        }
 
     # Create a new object from a python dictionary imported using json
     @classmethod
     def fromJSON(cls, jsondata):
         guilddata = Guild()
+        guilddata.id = jsondata["id"]
+        guilddata.small_edge = jsondata.get("small_edge")
+        guilddata.large_edge = jsondata.get("large_edge")
+        guilddata.high_limit = jsondata.get("high_limit")
+        guilddata.low_limit = jsondata.get("low_limit")
         return guilddata
 
 
@@ -55,3 +86,17 @@ def load(guildid):
     guild = Guild.fromJSON(jsondata)
 
     return guild
+
+
+def delete(guildid):
+    path = getGuildPath(guildid)
+    path.unlink(missing_ok = True)
+
+
+def exists(guildid):
+    exists = True
+    try:
+        load(guildid)
+    except errors.GuildNotFoundException:
+        exists = False
+    return exists
