74d6ea5334abc33a3c2d06b1b743d5c8e7a36c70
Natalie Fearnley <nfearnley@gmail.com>
2020-10-25 22:26:13 -0400

    Added unit parsing to sizebotapi

--- sizebotapi/main.py
+++ sizebotapi/main.py
@@ -1,10 +1,16 @@
+import asyncio
 import json
 
 import flask
-from flask import abort
+from flask import abort, request
 
 from sizebot.lib import userdb
-from sizebot.lib.errors import UserNotFoundException
+from sizebot.lib.errors import InvalidSizeValue, UserNotFoundException
+
+from sizebot.lib import units
+from sizebot.lib.units import SV, WV, TV
+
+asyncio.run(units.init())
 
 app = flask.Flask(__name__)
 
@@ -22,6 +28,36 @@ def user(guildid, userid):
         abort(404)
 
     return json.dumps({"nickname": u.nickname})
+
+
+@app.route("/unit/SV/parse", methods=["GET"])
+def SV_parse():
+    s = request.args.get("s")
+    try:
+        val = SV.parse(s)
+    except InvalidSizeValue:
+        abort(404)
+    return json.dumps({"SV": str(val)})
+
+
+@app.route("/unit/WV/parse", methods=["GET"])
+def WV_parse():
+    s = request.args.get("s")
+    try:
+        val = WV.parse(s)
+    except InvalidSizeValue:
+        abort(404)
+    return json.dumps({"WV": str(val)})
+
+
+@app.route("/unit/TV/parse", methods=["GET"])
+def TV_parse():
+    s = request.args.get("s")
+    try:
+        val = TV.parse(s)
+    except InvalidSizeValue:
+        abort(404)
+    return json.dumps({"TV": str(val)})
 
 
 def main():
