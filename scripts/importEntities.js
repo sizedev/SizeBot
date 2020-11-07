edd023fecdfbf8052477bfee8437ea35179979b5
Natalie Fearnley <nfearnley@gmail.com>
2020-11-07 04:55:13 -0500

    fix for macrovision height import script

--- scripts/importEntities.js
+++ scripts/importEntities.js
@@ -6,7 +6,7 @@ function importEntities() {
             model_heights[entity.name] = {};
             Object.entries(entity.views)
                 .forEach(function([name, view]) {
-                    model_heights[entity.name][name] = view.height.toNumber("meters");
+                    model_heights[entity.name][name] = view.attributes.height.base.toNumber("meters");
                 });
         });
     return JSON.stringify(model_heights, null, 4);
