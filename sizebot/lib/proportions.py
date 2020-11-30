745c629d71505e32e9681f13c028357daf9930e6
DigiDuncan <digiduncan@gmail.com>
2020-11-29 20:45:35 -0500

    test sqrt velocity

--- sizebot/lib/proportions.py
+++ sizebot/lib/proportions.py
@@ -408,7 +408,7 @@ class PersonStats:
 
         self.horizondistance = SV(math.sqrt(math.pow(self.height + 6378137, 2) - 40680631590769))
 
-        self.terminalvelocity = SV(defaultterminalvelocity * self.scale)
+        self.terminalvelocity = SV(defaultterminalvelocity * math.sqrt(self.scale))
         self.fallproof = self.terminalvelocity < falllimit
         self.fallproofcheck = emojis.voteyes if self.fallproof else emojis.voteno
 
