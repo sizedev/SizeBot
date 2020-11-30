c36181a15e4131a7ffdaa19ab614e530865a25a5
DigiDuncan <digiduncan@gmail.com>
2020-11-30 03:59:46 -0500

    add commands to royale

--- sizebot/cogs/royale.py
+++ sizebot/cogs/royale.py
@@ -51,15 +51,52 @@ class RoyaleCog(commands.Cog):
             except Exception:
                 await m.edit(content = f"Game failed to load.")
                 raise
-        
-        elif subcommand == "next":
+
+        else:
             if not ctx.guild.id in current_games:
                 await ctx.send("There is no game running in this guild!")
                 return
+        
+        if subcommand == "next":
             round = await current_games[ctx.guild.id].next()
             for e in round:
-                await ctx.send(embed = e.to_embed())
+                data = e.to_embed()
+                await ctx.send(embed = data[0], file = data[1])
                 await asyncio.sleep(1)
+
+        if subcommand == "overview":
+            data = current_games[ctx.guild.id].stats_screen().to_embed()
+            await ctx.send(embed = data[0], file = data[1])
+
+        if subcommand == "stop" or subcommand == "delete":
+            sentMsg = await ctx.send(f"{emojis.warning} **WARNING!** Deleting your game will remove *all progress irrecoverably.* Are you sure?"
+                f"To delete your game, react with {emojis.check}.")
+            await sentMsg.add_reaction(emojis.check)
+            await sentMsg.add_reaction(emojis.cancel)
+
+            # Wait for requesting user to react to sent message with emojis.check or emojis.cancel
+            def check(reaction, reacter):
+                return reaction.message.id == sentMsg.id \
+                    and reacter.id == user.id \
+                    and (
+                        str(reaction.emoji) == emojis.check
+                        or str(reaction.emoji) == emojis.cancel
+                    )
+
+            try:
+                reaction, user = await self.bot.wait_for("reaction_add", timeout=60.0, check=check)
+            except asyncio.TimeoutError:
+                # User took too long to respond
+                return
+            finally:
+                # User took too long OR User clicked the emoji
+                await sentMsg.delete()
+
+            # if the reaction isn't the right one, stop.
+            if reaction.emoji != emojis.check:
+                return
+
+            current_games.pop(ctx.guild.id)
 
 def setup(bot):
     bot.add_cog(RoyaleCog(bot))
