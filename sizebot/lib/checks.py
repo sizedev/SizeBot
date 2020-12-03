aa32b80c1668c1b7cb0585737a591413c97f2532
DigiDuncan <digiduncan@gmail.com>
2020-12-03 01:48:46 -0500

    various fixes

--- sizebot/lib/checks.py
+++ sizebot/lib/checks.py
@@ -5,7 +5,7 @@ def is_mod():
     async def predicate(ctx):
         author = ctx.author
         modness = False
-        if ctx.bot.is_owner(author):
+        if await ctx.bot.is_owner(author):
             modness = True
         elif author.permissions_in(ctx.channel).manage_guild:
             modness = True
