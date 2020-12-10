3760a12d4ee09a9f5926bab3a588c544cb699dce
DigiDuncan <digiduncan@gmail.com>
2020-12-09 20:36:31 -0500

    Digi also has to pee.

--- sizebot/cogs/fun.py
+++ sizebot/cogs/fun.py
@@ -1,7 +1,10 @@
 import asyncio
+import importlib.resources as pkg_resources
 
+from discord import File
 from discord.ext import commands
 
+import sizebot.data
 from sizebot.lib.constants import ids
 
 tasks = {}
@@ -58,6 +61,20 @@ class FunCog(commands.Cog):
         await ctx.message.delete(delay=0)
         newstring = f":musical_score: *{s}* :musical_note:"
         await ctx.send(newstring)
+
+    @commands.command(
+        hidden = True
+    )
+    async def digipee(self, ctx):
+        with pkg_resources.open_file(sizebot.data, "digipee.mp3") as f:
+            digipee = File(f)
+        await ctx.send(f"<@{ids.digiduncan}> also has to pee.", file = digipee)
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.content.startswith("!digipee"):
+            cont = await self.bot.get_context(message)
+            await cont.invoke(self.bot.get_command("digipee"))
 
 
 def setup(bot):
