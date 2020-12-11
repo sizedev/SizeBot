d68bf3d246a8d52c72836544da39b64b80ee2ccf
DigiDuncan <digiduncan@gmail.com>
2020-12-11 00:47:27 -0500

    fix cmd logging?

--- sizebot/lib/loglevels.py
+++ sizebot/lib/loglevels.py
@@ -12,7 +12,7 @@ def create_log_levels():
     BANNER = digilogger.addLogLevel("banner", fg="orange_red_1", bg="deep_sky_blue_4b", attr="bold")
     LOGIN = digilogger.addLogLevel("login", fg="cyan")
     EGG = digilogger.addLogLevel("egg", fg="magenta_2b", bg="light_yellow", attr="bold")
-    CMD = digilogger.addLogLevel("cmd", fg="grey_50", base="DEBUG")
+    CMD = digilogger.addLogLevel("cmd", fg="grey_50", base="DEBUG", showprefix = True, showtime = True)
     ROYALE = digilogger.addLogLevel("royale", fg="cyan")
 
 
