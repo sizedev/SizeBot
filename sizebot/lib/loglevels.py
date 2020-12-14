861c78ff1df7eb2990e3376fb33bd6204e3a3c48
DigiDuncan <digiduncan@gmail.com>
2020-12-13 21:10:45 -0500

    prefixes

--- sizebot/lib/loglevels.py
+++ sizebot/lib/loglevels.py
@@ -9,11 +9,11 @@ ROYALE = None
 
 def create_log_levels():
     global BANNER, LOGIN, EGG, CMD, ROYALE
-    BANNER = digilogger.addLogLevel("banner", fg="orange_red_1", bg="deep_sky_blue_4b", attr="bold")
+    BANNER = digilogger.addLogLevel("banner", fg="orange_red_1", bg="deep_sky_blue_4b", attr="bold", showtime = False)
     LOGIN = digilogger.addLogLevel("login", fg="cyan")
-    EGG = digilogger.addLogLevel("egg", fg="magenta_2b", bg="light_yellow", attr="bold")
-    CMD = digilogger.addLogLevel("cmd", fg="grey_50", base="DEBUG", showprefix = True, showtime = True)
-    ROYALE = digilogger.addLogLevel("royale", fg="cyan")
+    EGG = digilogger.addLogLevel("egg", fg="magenta_2b", bg="light_yellow", attr="bold", prefix="EGG")
+    CMD = digilogger.addLogLevel("cmd", fg="grey_50", base="DEBUG", prefix="CMD")
+    ROYALE = digilogger.addLogLevel("royale", fg="cyan", prefix="ROYALE")
 
 
 create_log_levels()
