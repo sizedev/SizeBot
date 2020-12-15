497d04e9e1f4942037f743adcd37b924668a1258
DigiDuncan <digiduncan@gmail.com>
2020-12-15 02:51:40 -0500

    Maybe fix the loading bar bug?

--- sizebot/cogs/royale.py
+++ sizebot/cogs/royale.py
@@ -105,7 +105,8 @@ class RoyaleCog(commands.Cog):
                     looppoint = arrow.now()
                     if looppoint - loop >= timedelta(seconds = 1):
                         loop = looppoint
-                        await m.edit(content = f"`{progress}` {emojis.loading}")
+                        if progress:
+                            await m.edit(content = f"`{progress}` {emojis.loading}")
                 if game.royale.parser.errors:
                     await ctx.send(f"{emojis.warning} **Errors in parsing:**\n"
                                    + ("\n".join(game.royale.parser.errors)))
