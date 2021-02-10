a86f66f1096d66c13f01b64650f5c36786793a0c
DigiDuncan <digiduncan@gmail.com>
2021-02-10 18:16:03 -0500

    update version number
    actually remember to do this more

--- sizebot/__init__.py
+++ sizebot/__init__.py
@@ -1,4 +1,4 @@
 # Version (https://www.python.org/dev/peps/pep-0440/#public-version-identifiers)
 # Release versions: [major].[minor].[micro]
 # Alpha versions: [major].[minor].[micro].dev[0,1,2...]
-__version__ = "3.5.5"
+__version__ = "3.5.8"
