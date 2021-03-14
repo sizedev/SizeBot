34dd77ff96c2179f75519977484426f8484f2f5d
Natalie Fearnley <nfearnley@gmail.com>
2021-03-14 04:52:30 -0400

    cleaned up say command, and added tests for default say

--- tests/test_say.py
+++ tests/test_say.py
@@ -2,24 +2,27 @@ import pytest
 from unittest.mock import AsyncMock, MagicMock, patch
 from sizebot.cogs import say
 from sizebot.lib.units import SV
+from sizebot.lib.errors import UserNotFoundException
 
 
 @pytest.fixture
 def ctx():
-    mock = AsyncMock()
-    return mock
+    ctx = AsyncMock()
+    ctx.guild.id = 1
+    ctx.author.id = 2
+    ctx.author.display_name = "defaultboi"
+    return ctx
 
 
 @pytest.fixture
 def userdb_load():
     with patch("sizebot.lib.userdb.load") as userdb_load:
+        userdb_load.defaultheight = SV("1.754")
         yield userdb_load
 
 
 @pytest.mark.asyncio
-async def test_say_smollest(ctx, userdb_load):
-    ctx.guild.id = 1
-    ctx.author.id = 2
+async def test_say_n3(ctx, userdb_load):
     userdb_load.return_value = MagicMock(height=SV("0.001"), nickname="smollestboi")
     await say.SayCog.say(None, ctx, message="Hello, World!")
     userdb_load.assert_called_with(1, 2)
@@ -28,9 +31,7 @@ async def test_say_smollest(ctx, userdb_load):
 
 
 @pytest.mark.asyncio
-async def test_say_smoller(ctx, userdb_load):
-    ctx.guild.id = 1
-    ctx.author.id = 2
+async def test_say_n2(ctx, userdb_load):
     userdb_load.return_value = MagicMock(height=SV("0.008"), nickname="smollerboi")
     await say.SayCog.say(None, ctx, message="Hello, World!")
     userdb_load.assert_called_with(1, 2)
@@ -39,9 +40,7 @@ async def test_say_smoller(ctx, userdb_load):
 
 
 @pytest.mark.asyncio
-async def test_say_smol(ctx, userdb_load):
-    ctx.guild.id = 1
-    ctx.author.id = 2
+async def test_say_n1(ctx, userdb_load):
     userdb_load.return_value = MagicMock(height=SV("0.1"), nickname="smolboi")
     await say.SayCog.say(None, ctx, message="Hello, World!")
     userdb_load.assert_called_with(1, 2)
@@ -50,9 +49,7 @@ async def test_say_smol(ctx, userdb_load):
 
 
 @pytest.mark.asyncio
-async def test_say_normal(ctx, userdb_load):
-    ctx.guild.id = 1
-    ctx.author.id = 2
+async def test_say_p0(ctx, userdb_load):
     userdb_load.return_value = MagicMock(height=SV("2"), nickname="boi")
     await say.SayCog.say(None, ctx, message="Hello, World!")
     userdb_load.assert_called_with(1, 2)
@@ -61,9 +58,7 @@ async def test_say_normal(ctx, userdb_load):
 
 
 @pytest.mark.asyncio
-async def test_say_tol(ctx, userdb_load):
-    ctx.guild.id = 1
-    ctx.author.id = 2
+async def test_say_p1(ctx, userdb_load):
     userdb_load.return_value = MagicMock(height=SV("20"), nickname="tolboi")
     await say.SayCog.say(None, ctx, message="Hello, World!")
     userdb_load.assert_called_with(1, 2)
@@ -72,9 +67,7 @@ async def test_say_tol(ctx, userdb_load):
 
 
 @pytest.mark.asyncio
-async def test_say_toller(ctx, userdb_load):
-    ctx.guild.id = 1
-    ctx.author.id = 2
+async def test_say_p2(ctx, userdb_load):
     userdb_load.return_value = MagicMock(height=SV("200"), nickname="tollerboi")
     await say.SayCog.say(None, ctx, message="Hello, World!")
     userdb_load.assert_called_with(1, 2)
@@ -83,11 +76,18 @@ async def test_say_toller(ctx, userdb_load):
 
 
 @pytest.mark.asyncio
-async def test_say_tollest(ctx, userdb_load):
-    ctx.guild.id = 1
-    ctx.author.id = 2
+async def test_say_p3(ctx, userdb_load):
     userdb_load.return_value = MagicMock(height=SV("2000"), nickname="tollestboi")
     await say.SayCog.say(None, ctx, message="Hello, World!")
     userdb_load.assert_called_with(1, 2)
     ctx.message.delete.assert_called_with(delay=0)
     ctx.send.assert_called_with("tollestboi booms: \n> :regional_indicator_h::regional_indicator_e::regional_indicator_l::regional_indicator_l::regional_indicator_o::arrow_up_small:<:blank:665063842866397185>:regional_indicator_w::regional_indicator_o::regional_indicator_r::regional_indicator_l::regional_indicator_d::exclamation:")
+
+
+@pytest.mark.asyncio
+async def test_say_default(ctx, userdb_load):
+    userdb_load.side_effect = UserNotFoundException(1, 2)
+    await say.SayCog.say(None, ctx, message="Hello, World!")
+    userdb_load.assert_called_with(1, 2)
+    ctx.message.delete.assert_called_with(delay=0)
+    ctx.send.assert_called_with("defaultboi says: \n> Hello, World!")
