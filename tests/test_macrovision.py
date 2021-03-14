96c7d415de129f62fc092e10f1e1c44046521bae
Natalie Fearnley <nfearnley@gmail.com>
2021-03-13 20:49:07 -0500

    Fixed macrovision tests

--- tests/test_macrovision.py
+++ tests/test_macrovision.py
@@ -1,28 +1,82 @@
+import json
+import base64
+
 import pytest
 
 from sizebot.lib import macrovision, units
 from sizebot.lib.digidecimal import Decimal
 from sizebot.lib.units import SV
 
+
 units.init()
+
+
+def json_to_base64(j):
+    json_string = json.dumps(j)
+    json_bytes = json_string.encode("utf-8")
+    base64_bytes = base64.b64encode(json_bytes)
+    base64_string = base64_bytes.decode("utf-8")
+    return base64_string
+
 
 @pytest.mark.asyncio
 async def test_macrovision():
-    assert await macrovision.get_url([
-        {"name": "Duncan", "model": "man1", "height": Decimal("0.0127")},
-        {"name": "Natalie", "model": "woman1", "height": Decimal("0.1524")}
-    ], shorten = False) == "https://macrovision.crux.sexy/?scene=eyJlbnRpdGllcyI6IFt7Im5hbWUiOiAiSHVtYW4iLCAiY3VzdG9tTmFtZSI6ICJOYXRhbGllIiwgInNjYWxlIjogMC4wODk1NTIyMzg4MDU5NzAxNCwgInZpZXciOiAid29tYW4xIiwgIngiOiAiMCIsICJ5IjogIjAiLCAicHJpb3JpdHkiOiAwLCAiYnJpZ2h0bmVzcyI6IDF9LCB7Im5hbWUiOiAiSHVtYW4iLCAiY3VzdG9tTmFtZSI6ICJEdW5jYW4iLCAic2NhbGUiOiAwLjAwNzA0MjI1MzUyMTEyNjc2LCAidmlldyI6ICJtYW4xIiwgIngiOiAiMC4wMzgxIiwgInkiOiAiMCIsICJwcmlvcml0eSI6IDAsICJicmlnaHRuZXNzIjogMX1dLCAid29ybGQiOiB7ImhlaWdodCI6IDAuMTUyNCwgInVuaXQiOiAibWV0ZXJzIiwgIngiOiAiMCIsICJ5IjogIjAifSwgInZlcnNpb24iOiAzfQ=="
+    expected_base64 = json_to_base64({
+        "entities": [
+            {"name": "Human", "customName": "Natalie", "scale": 0.08955223880597014, "view": "woman1", "x": "0", "y": "0", "priority": 0, "brightness": 1},
+            {"name": "Human", "customName": "Duncan", "scale": 0.00704225352112676, "view": "man1", "x": "0.0381", "y": "0", "priority": 0, "brightness": 1}
+        ],
+        "world": {"height": 0.1524, "unit": "meters", "x": 0, "y": 0},
+        "version": 3
+    })
+    expected_url = f"https://macrovision.crux.sexy/?scene={expected_base64}"
+    macrovision_url = await macrovision.get_url(
+        [
+            {"name": "Duncan", "model": "man1", "height": Decimal("0.0127")},
+            {"name": "Natalie", "model": "woman1", "height": Decimal("0.1524")}
+        ],
+        shorten = False
+    )
+    assert macrovision_url == expected_url
+
 
 @pytest.mark.asyncio
 async def test_macrovision_SV():
-    assert await macrovision.get_url([
-        {"name": "Duncan", "model": "man1", "height": SV.parse("0.5in")},
-        {"name": "Natalie", "model": "woman1", "height": SV.parse("6in")}
-    ], shorten = False) == "https://macrovision.crux.sexy/?scene=eyJlbnRpdGllcyI6IFt7Im5hbWUiOiAiSHVtYW4iLCAiY3VzdG9tTmFtZSI6ICJOYXRhbGllIiwgInNjYWxlIjogMC4wODk1NTIyMzg4MDU5NzAxNCwgInZpZXciOiAid29tYW4xIiwgIngiOiAiMCIsICJ5IjogIjAiLCAicHJpb3JpdHkiOiAwLCAiYnJpZ2h0bmVzcyI6IDF9LCB7Im5hbWUiOiAiSHVtYW4iLCAiY3VzdG9tTmFtZSI6ICJEdW5jYW4iLCAic2NhbGUiOiAwLjAwNzA0MjI1MzUyMTEyNjc2LCAidmlldyI6ICJtYW4xIiwgIngiOiAiMC4wMzgxIiwgInkiOiAiMCIsICJwcmlvcml0eSI6IDAsICJicmlnaHRuZXNzIjogMX1dLCAid29ybGQiOiB7ImhlaWdodCI6IDAuMTUyNCwgInVuaXQiOiAibWV0ZXJzIiwgIngiOiAiMCIsICJ5IjogIjAifSwgInZlcnNpb24iOiAzfQ=="
+    expected_base64 = json_to_base64({
+        "entities": [
+            {"name": "Human", "customName": "Natalie", "scale": 0.08955223880597014, "view": "woman1", "x": "0", "y": "0", "priority": 0, "brightness": 1},
+            {"name": "Human", "customName": "Duncan", "scale": 0.00704225352112676, "view": "man1", "x": "0.0381", "y": "0", "priority": 0, "brightness": 1}
+        ],
+        "world": {"height": 0.1524, "unit": "meters", "x": 0, "y": 0},
+        "version": 3
+    })
+    expected_url = f"https://macrovision.crux.sexy/?scene={expected_base64}"
+    macrovision_url = await macrovision.get_url(
+        [
+            {"name": "Duncan", "model": "man1", "height": SV.parse("0.5in")},
+            {"name": "Natalie", "model": "woman1", "height": SV.parse("6in")}
+        ],
+        shorten = False
+    )
+    assert macrovision_url == expected_url
+
 
 @pytest.mark.asyncio
 async def test_weird_names():
-    assert await macrovision.get_url([
-        {"name": r"r'(?<!\.)[.?!](?!\.)', z [1.22m]", "model": "man1", "height": Decimal("1.219")},
-        {"name": "Natalie", "model": "woman1", "height": Decimal("0.1524")}
-    ], shorten = False) == "https://macrovision.crux.sexy/?scene=eyJlbnRpdGllcyI6IFt7Im5hbWUiOiAiSHVtYW4iLCAiY3VzdG9tTmFtZSI6ICJyJyg/PCFcXC4pWy4/IV0oPyFcXC4pJywgeiBbMS4yMm1dIiwgInNjYWxlIjogMC42NzU5NDU0MzYzOTc5MTUsICJ2aWV3IjogIm1hbjEiLCAieCI6ICIwIiwgInkiOiAiMCIsICJwcmlvcml0eSI6IDAsICJicmlnaHRuZXNzIjogMX0sIHsibmFtZSI6ICJIdW1hbiIsICJjdXN0b21OYW1lIjogIk5hdGFsaWUiLCAic2NhbGUiOiAwLjA4OTU1MjIzODgwNTk3MDE0LCAidmlldyI6ICJ3b21hbjEiLCAieCI6ICIwLjMwNDc1IiwgInkiOiAiMCIsICJwcmlvcml0eSI6IDAsICJicmlnaHRuZXNzIjogMX1dLCAid29ybGQiOiB7ImhlaWdodCI6IDEuMjE5LCAidW5pdCI6ICJtZXRlcnMiLCAieCI6ICIwIiwgInkiOiAiMCJ9LCAidmVyc2lvbiI6IDN9"
+    expected_base64 = json_to_base64({
+        "entities": [
+            {"name": "Human", "customName": r"r'(?<!\.)[.?!](?!\.)', z [1.22m]", "scale": 0.675945436397915, "view": "man1", "x": "0", "y": "0", "priority": 0, "brightness": 1},
+            {"name": "Human", "customName": "Natalie", "scale": 0.08955223880597014, "view": "woman1", "x": "0.30475", "y": "0", "priority": 0, "brightness": 1}
+        ],
+        "world": {"height": 1.219, "unit": "meters", "x": 0, "y": 0},
+        "version": 3
+    })
+    expected_url = f"https://macrovision.crux.sexy/?scene={expected_base64}"
+    macrovision_url = await macrovision.get_url(
+        [
+            {"name": r"r'(?<!\.)[.?!](?!\.)', z [1.22m]", "model": "man1", "height": Decimal("1.219")},
+            {"name": "Natalie", "model": "woman1", "height": Decimal("0.1524")}
+        ],
+        shorten = False
+    )
+    assert macrovision_url == expected_url
