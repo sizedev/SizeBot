67c26b573552d33217cbd7e4aa502cbb679562c7
DigiDuncan <digiduncan@gmail.com>
2022-01-20 22:08:35 -0500

    &pokecomp

--- sizebot/cogs/pokemon.py
+++ sizebot/cogs/pokemon.py
@@ -2,9 +2,13 @@ import logging
 import random
 import typing
 
+import discord
 from discord.ext import commands
 
+from sizebot.lib import userdb
+from sizebot.lib.fakeplayer import FakePlayer
 from sizebot.lib.pokemon import pokemon
+from sizebot.lib.units import SV
 
 logger = logging.getLogger("sizebot")
 
@@ -14,10 +18,10 @@ class PokemonCog(commands.Cog):
         self.bot = bot
 
     @commands.command(
-        aliases = ["pkmn"],
+        aliases = ["dex"],
         category = "objects"
     )
-    async def pokemon(self, ctx, pkmn: typing.Union[int, str] = None):
+    async def pokedex(self, ctx, pkmn: typing.Union[int, str] = None):
         """Pokemaaaaaaaaans"""
         if isinstance(pkmn, str):
             p = next((m for m in pokemon if m.name.lower() == pkmn.lower()), None)
@@ -30,6 +34,31 @@ class PokemonCog(commands.Cog):
             p = random.choice(pokemon)
 
         e = p.stats_embed()
+        await ctx.send(embed = e)
+
+    @commands.command(
+        aliases = ["pokecompare", "pokecomp", "lookatpoke"],
+        category = "objects"
+    )
+    async def lookatpokemon(self, ctx, pkmn: typing.Union[int, str] = None, *, who: typing.Union[discord.Member, FakePlayer, SV] = None):
+        """Pokemaaaaaaaaans"""
+        if who is None:
+            who = ctx.author
+
+        userdata = userdb.load_or_fake(who)
+
+        if isinstance(pkmn, str):
+            p = next((m for m in pokemon if m.name.lower() == pkmn.lower()), None)
+        elif isinstance(pkmn, int):
+            p = next((m for m in pokemon if m.natdex == pkmn), None)
+        else:
+            p = None
+
+        if p is None:
+            p = random.choice(pokemon)
+
+        e = p.stats_embed(multiplier = userdata.viewscale)
+        e.title += f" to {userdata.nickname}"
         await ctx.send(embed = e)
 
 
