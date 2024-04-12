9fcba7d3b357983e41e712c1c019818ef199e1c0
DigiDuncan <digiduncan@gmail.com>
2024-04-11 20:47:26 -0400

    logging woah

--- sizebot/lib/statproxy.py
+++ sizebot/lib/statproxy.py
@@ -1,8 +1,11 @@
+import logging
 from sizebot.lib.errors import InvalidStat, InvalidStatTag
 from sizebot.lib.stats import statmap, taglist
 
 re_full_string = r"\$(\w+=[^;$]+;?)+"
 re_component = r"(\w+)=([^;$]+);?"
+
+logger = logging.getLogger("sizebot")
 
 
 class StatProxy:
@@ -18,18 +21,26 @@ class StatProxy:
     
     @classmethod
     def parse(cls, s: str):
+        logger.info(f"{s}: checking StatProxy...")
         tag = False
         if s.startswith("#"):
+            logger.info("Looks like a tag.")
             tag = True
             s = s.removeprefix("#")
 
         if tag:
             if s not in taglist:
+                logger.info(f"Looks like {s} isn't a tag.")
+                logger.info(f"It's not in this list: {taglist}")
                 raise InvalidStatTag(s)
+            logger.info("Check passed!")
             return StatProxy(s, True)
         else:
-            if s not in statmap:
+            if s not in statmap.keys():
+                logger.info(f"Looks like {s} isn't a stat.")
+                logger.info(f"It's not in this list: {statmap.keys()}")
                 raise InvalidStat(s)
+            logger.info("Check passed!")
             return StatProxy(statmap[s], False)
 
     @classmethod
