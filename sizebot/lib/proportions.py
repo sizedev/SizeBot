a3a1e0ee8f5385f3cb8e8d338b495c12039faa57
DigiDuncan <digiduncan@gmail.com>
2024-04-23 05:45:19 -0400

    better logging

--- sizebot/lib/proportions.py
+++ sizebot/lib/proportions.py
@@ -157,8 +157,8 @@ class PersonComparison:  # TODO: Make a one-sided comparison option.
         
 
         for sstat, bstat in zip(self.smallToBig.stats, self.bigToSmall.stats):
+            logger.info(f"{sstat.key}, {bstat.key} [{(sstat.is_shown or bstat.is_shown)}]: {sstat.value}, {bstat.value}")
             if (sstat.is_shown or bstat.is_shown) and (sstat.value is not None or bstat.value is not None):
-                logger.info(f"{sstat.key}, {bstat.key}: {sstat.value}, {bstat.value}")
                 embed.add_field(name = sstat.title, value = self.get_single_body(sstat.key), inline = True)
 
         embed.add_field(name=f"{emojis.comparebig} Speeds", value=self.bigToSmall.simplespeeds, inline=False)
