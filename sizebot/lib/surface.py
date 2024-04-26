a58ec34336841e291fe8f2b909fe9e014b49be4d
DigiDuncan <digiduncan@gmail.com>
2024-04-26 06:28:40 -0400

    float math

--- sizebot/lib/surface.py
+++ sizebot/lib/surface.py
@@ -6,6 +6,6 @@ WATER_SURFACE_TENSION = 72.8  # mN/m
 
 def can_walk_on_water(weight: WV, footlength: SV, footwidth: SV) -> bool:
     foot_cirum = (footlength * 2) + (footwidth * 2)
-    foot_force_frac = ((weight / 1000) * GRAVITY) / (WATER_SURFACE_TENSION * foot_cirum * 2)
+    foot_force_frac = ((float(weight) / 1000) * GRAVITY) / (WATER_SURFACE_TENSION * float(foot_cirum) * 2)
     angle = acos(foot_force_frac)
     return angle < (pi / 2)
