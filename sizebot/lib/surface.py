b3ada20c94451fc14c2651ab128812ac31b48b83
DigiDuncan <digiduncan@gmail.com>
2024-04-26 06:40:59 -0400

    fix units

--- sizebot/lib/surface.py
+++ sizebot/lib/surface.py
@@ -2,7 +2,7 @@ from math import acos, pi
 from sizebot.lib.units import SV, WV
 
 GRAVITY = 9.807  # m/s^2
-WATER_SURFACE_TENSION = 72.8  # mN/m
+WATER_SURFACE_TENSION = 0.0728  # N/m
 
 def can_walk_on_water(weight: WV, footlength: SV, footwidth: SV) -> bool:
     foot_cirum = (footlength * 2) + (footwidth * 2)
