a67a7eec7d9d4eb8051f94ae180ab996fd99f3c8
DigiDuncan <digiduncan@gmail.com>
2024-04-26 06:55:55 -0400

    better math?

--- sizebot/lib/surface.py
+++ sizebot/lib/surface.py
@@ -1,10 +1,18 @@
+import logging
+
 from math import acos, pi
 from sizebot.lib.units import SV, WV
 
-GRAVITY = 9.807  # m/s^2
-WATER_SURFACE_TENSION = 0.0728  # N/m
+GRAVITY = 980.7  # cm/s^2
+WATER_SURFACE_TENSION = 72.8  # dy/cm
+
+logger = logging.getLogger("sizebot")
+
+# WV = grams
+# SV = meters
 
 def can_walk_on_water(weight: WV, footlength: SV, footwidth: SV) -> bool:
-    foot_cirum = (footlength * 2) + (footwidth * 2)
-    foot_force_frac = ((float(weight) / 1000) * GRAVITY) / (WATER_SURFACE_TENSION * float(foot_cirum) * 2)
+    foot_cirum = (footlength * 2 * 100) + (footwidth * 2 * 100)
+    foot_force_frac = (float(weight) * GRAVITY) / (WATER_SURFACE_TENSION * float(foot_cirum) * 2)
+    logger.info(f"{foot_force_frac=}")
     return 0 <= foot_force_frac < 1
