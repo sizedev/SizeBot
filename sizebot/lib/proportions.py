4a881939a29cf391abdeae51c26a962a8ee79552
DigiDuncan <digiduncan@gmail.com>
2024-04-27 01:20:04 -0400

    update changelog

--- sizebot/lib/proportions.py
+++ sizebot/lib/proportions.py
@@ -302,7 +302,7 @@ class PersonSpeedComparison:
         # value=(speedcalc(self.viewer, self.viewed["height"].value))
         embed.set_author(name=f"SizeBot {__version__}", icon_url=compareicon)
         embed.add_field(name=f"**{self.viewer["nickname"].value}** Speeds", value=self.viewer.stats_by_key["simplespeeds+"].body, inline=False)
-        
+
         embed.add_field(name="Height", value=speedcalc(self.viewer, self.viewed["height"].value, include_relative = True), inline=True)  # hardcode height because it's weird
         embed.add_field(name="Foot Length", value=speedcalc(self.viewer, self.viewed["footlength"].value, include_relative = True, foot = True), inline=True)  # hardcode height because it's weird
         for stat in self.viewed.stats:
