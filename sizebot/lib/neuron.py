97ee96b8608649a266d3a9f0044658c666985676
DigiDuncan <digiduncan@gmail.com>
2024-05-02 20:27:37 -0400

    neuron accuracy

--- sizebot/lib/neuron.py
+++ sizebot/lib/neuron.py
@@ -31,6 +31,6 @@ def get_neuron_embed(userdata: User) -> Embed:
     e = Embed(title = f"Neuron Travel Distance for {statbox.stats_by_key['nickname'].value}",
               description = f"{statbox.stats_by_key['nickname'].value} is {statbox.stats_by_key['height'].value:.1mu} tall.")
     for part in get_args(Part):
-        e.add_field(name = part.title(), value = pretty_time_delta(get_speed_to_part(statbox, part)))
+        e.add_field(name = part.title(), value = pretty_time_delta(get_speed_to_part(statbox, part), millisecondAccuracy = True, roundeventually = True))
 
     return e
