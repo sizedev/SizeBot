81d6fafcca166f21b61ebad3b253eeabf2574b72
Natalie Fearnley <nfearnley@gmail.com>
2024-05-04 00:47:18 -0400

    merging stats

---
+++ sizebot/lib/surface.py
@@ -0,0 +1,17 @@
+import logging
+from sizebot.lib.units import SV, WV
+
+GRAVITY = 980.7  # cm/s^2
+WATER_SURFACE_TENSION = 72.8  # dy/cm
+
+logger = logging.getLogger("sizebot")
+
+# WV = grams
+# SV = meters
+
+
+def can_walk_on_water(weight: WV, footlength: SV, footwidth: SV) -> bool:
+    foot_cirum = (footlength * 2 * 100) + (footwidth * 2 * 100)
+    foot_force_frac = (float(weight) * GRAVITY) / (WATER_SURFACE_TENSION * float(foot_cirum) * 2)
+    r = bool(0 <= foot_force_frac < 1)
+    return r
