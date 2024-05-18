91ea9861fe4654554fef9e87937d7b04e67a6bdb
Natalie Fearnley <nfearnley@gmail.com>
2024-05-17 22:22:32 -0400

    fix for triggers being set on unregistered users

--- sizebot/cogs/trigger.py
+++ sizebot/cogs/trigger.py
@@ -40,8 +40,9 @@ def unset_cached_trigger(guildid, authorid, trigger):
 
 
 def set_trigger(guildid, authorid, trigger, diff):
-    set_cached_trigger(guildid, authorid, trigger, diff)
     userdata = userdb.load(guildid, authorid)
+    # Only set the cache _after_ we've check if the user is registered
+    set_cached_trigger(guildid, authorid, trigger, diff)
     userdata.triggers[trigger] = diff
     userdb.save(userdata)
 
