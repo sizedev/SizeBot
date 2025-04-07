f5f625de54c034a7521ee7fa4df4c8d0cebf60c4
DigiDuncan <digiduncan@gmail.com>
2025-04-07 14:48:41 -0400

    format users better?

--- sizebot/main.py
+++ sizebot/main.py
@@ -243,7 +243,8 @@ def main():
     async def sb(interaction: discord.Interaction, command: str, arguments: Optional[str]):
         message = await interaction.response.send_message(f"{constants.emojis.loading} Processing command...", delete_after=0.5)
         full_command = command if not arguments else f"{command} {arguments}"
-        message = await interaction.channel.send(f"> **{interaction.user.display_name}** ran `{full_command}`.")
+        full_command_formatted = full_command.replace("<", "`<").replace(">", "`>")
+        message = await interaction.channel.send(f"> **{interaction.user.display_name}** ran `{full_command_formatted}`.")
         new_message = copy(message)
         new_message.author = interaction.user
         new_message.content = conf.prefix + full_command
